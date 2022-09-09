9a74fc3939bce3e86fbee4204b4d443cec0773f4
martin.dietrich <martin.dietrich@muenchen.de>
2022-09-09 12:00:51 +0200

    cleanup

--- digiwf-libs/digiwf-json-serialization/digiwf-json-serialization-core/src/main/java/io/muenchendigital/digiwf/json/validation/JsonSchemaValidator.java
+++ digiwf-libs/digiwf-json-serialization/digiwf-json-serialization-core/src/main/java/io/muenchendigital/digiwf/json/validation/JsonSchemaValidator.java
@@ -14,7 +14,6 @@ import org.json.JSONObject;
 
 import java.util.Collection;
 import java.util.List;
-
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -46,7 +45,6 @@ public class JsonSchemaValidator {
      */
     public void validate(final String schema, final Map<String, Object> data) {
         final Schema schemaObj = this.createSchema(new JSONObject(schema));
-//        schemaObj.validate(new JSONObject(data));
         Validator validator = buildValidator();
         validator.performValidation(schemaObj, data);
     }
@@ -82,7 +80,6 @@ public class JsonSchemaValidator {
 
     private void validate(final Map<String, Object> schemaObject, final JSONObject data) {
         final Schema schema = this.createSchema(new JSONObject(schemaObject));
-//        schema.validate(data);
         Validator validator = buildValidator();
         validator.performValidation(schema, data);
     }
