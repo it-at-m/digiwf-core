5a4240cd276bf8337de224937f2d22da6dc2f69d
martin.dietrich <martin.dietrich@muenchen.de>
2022-11-25 12:55:44 +0100

    fixed apiexception

--- digiwf-libs/digiwf-spring-cloudstream-utils/digiwf-spring-cloudstream-utils-core/src/main/java/io/muenchendigital/digiwf/spring/cloudstream/utils/api/streaming/incident/service/IncidentService.java
+++ digiwf-libs/digiwf-spring-cloudstream-utils/digiwf-spring-cloudstream-utils-core/src/main/java/io/muenchendigital/digiwf/spring/cloudstream/utils/api/streaming/incident/service/IncidentService.java
@@ -33,7 +33,7 @@ public class IncidentService {
                 .withPayload(errorMessage)
                 .setHeader(StreamingHeaders.TYPE, MESSAGE_TYPE)
                 .setHeader(DIGIWF_PROCESS_INSTANCE_ID, messageHeaders.get(DIGIWF_PROCESS_INSTANCE_ID))
-                .setHeader(DIGIWF_MESSAGE_NAME, "incident")
+                .setHeader(DIGIWF_MESSAGE_NAME, messageHeaders.get(DIGIWF_MESSAGE_NAME)) // bpmn token is waiting on that message receive event. Thats where we want our incident to be placed.
                 .setHeader("processInstanceId", messageHeaders.get(DIGIWF_PROCESS_INSTANCE_ID))
                 .build();
 
