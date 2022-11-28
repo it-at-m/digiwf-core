6417a15f95c63336c3fd06f86a964a3e9e2f911c
martin.dietrich <martin.dietrich@muenchen.de>
2022-11-28 11:58:31 +0100

    create-incident srv impl.

--- digiwf-engine/digiwf-engine-service/src/main/java/io/muenchendigital/digiwf/input/incident/domain/service/EngineIncidentService.java
+++ digiwf-engine/digiwf-engine-service/src/main/java/io/muenchendigital/digiwf/input/incident/domain/service/EngineIncidentService.java
@@ -6,11 +6,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.runtime.EventSubscription;
-import org.camunda.bpm.engine.runtime.Execution;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import javax.validation.Valid;
 
 @Slf4j
 @Service
@@ -18,48 +17,28 @@ import java.util.List;
 @RequiredArgsConstructor
 public class EngineIncidentService {
 
+    private static final String INCIDENT_TYPE   = "integrationError";
+    private static final String EVENT_TYPE      = "message";
+
     private final RuntimeService runtimeService;
 
-    public void createIncident(final Incident incident) {
+    public void createIncident(@Valid final Incident incident) {
         log.debug("createIncident {}", incident);
 
-        final List<EventSubscription> list = runtimeService.createEventSubscriptionQuery().eventName(incident.getMessageName()).processInstanceId(incident.getProcessInstanceId()).list();
-        log.debug("EventSubscriptions: {}", list.size());
-        if (list.isEmpty()){
-            log.error("No event subscriptions found to create incident for instance: {}", incident.getProcessInstanceId());
-            return;
-        }
-        list.forEach(s -> System.out.println("Activity: " + s));
-        final String activity = list.get(0).getActivityId();
+        //load corresponding event subscription
+        final String executionId = runtimeService.createEventSubscriptionQuery()
+                .eventName(incident.getMessageName())
+                .eventType(EVENT_TYPE)
+                .processInstanceId(incident.getProcessInstanceId()).list()
+                .stream()
+                .findFirst()
+                .map(EventSubscription::getExecutionId)
+                .orElseThrow();
 
-//        Incident incident = runtimeService.createIncident(Incident.FAILED_JOB_HANDLER_TYPE, incident.getProcessInstanceId(), activity, incident.getMessageName());
+        // create incident
+        runtimeService.createIncident(INCIDENT_TYPE, executionId, null, incident.getErrorMessage());
 
-//        String execId = list.get(0).getExecutionId();
-//        Execution exec = runtimeService.createExecutionQuery().executionId(execId).singleResult();
-//        exec.c
-
-//        Incident incident = runtimeService.createIncident("anytype", incident.getProcessInstanceId(), activity, incident.getMessageName());
-//        Incident incident = runtimeService.createIncident("anytype", incident.getProcessInstanceId(), list.get(0).getEventName(), incident.getMessageName());
-
-        final List<Execution> e = runtimeService.createExecutionQuery().processInstanceId(incident.getProcessInstanceId()).list();
-        e.forEach(e2 -> System.out.println(e2.getClass().getName()));
-        System.out.println("ex: " + e.getClass().getName());
-
-        for (final Execution ex : e) {
-            if (ex.getId().equals(incident.getProcessInstanceId())) {
-                continue;
-            }
-            try {
-//                System.out.println("ex: " + ex.getId()+" / " + ex.getProcessInstanceId());
-                runtimeService.createIncident("custom", ex.getId(), "foo", "bar");
-                System.out.println("EXEC: " + ex.getId());
-                return;
-            } catch (final Exception e1) {
-                System.out.println("exception: " + ex.getId());
-            }
-        }
-
-//        log.info("Incident created: {}", incident);
+        log.info("Incident created: {}", incident);
     }
 
 }
