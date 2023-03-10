d9c87d749f683d6c451bfd5b40db654bb6de3433
dominikhorn93 <dominik.horn93@gmail.com>
2023-03-10 11:15:47 +0100

    fix: s3 resolver as bean

--- digiwf-engine/digiwf-engine-service/src/main/java/io/muenchendigital/digiwf/legacy/dms/shared/S3Resolver.java
+++ digiwf-engine/digiwf-engine-service/src/main/java/io/muenchendigital/digiwf/legacy/dms/shared/S3Resolver.java
@@ -1,6 +1,7 @@
 package io.muenchendigital.digiwf.legacy.dms.shared;
 
 import org.apache.tika.Tika;
+import org.springframework.stereotype.Component;
 
 import javax.mail.util.ByteArrayDataSource;
 import java.io.IOException;
@@ -8,9 +9,10 @@ import java.io.InputStream;
 import java.net.URL;
 
 
+@Component
 public class S3Resolver {
 
-    public static byte[] getS3File(final String s3Url) throws IOException {
+    public byte[] getS3File(final String s3Url) throws IOException {
         final byte[] documentContent;
         final URL binaryFile = new URL(s3Url);
         final Tika tika = new Tika();
