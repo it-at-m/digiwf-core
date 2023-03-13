de0eef87f959ec61a4d14bc52dad9733dead8f34
Simon Zambrovski <simon.zambrovski@holisticon.de>
2023-03-13 21:35:55 +0100

    chore: define common task variables in API module

--- digiwf-task/digiwf-task-api/src/main/java/io/muenchendigital/digiwf/task/TaskVariables.java
+++ digiwf-task/digiwf-task-api/src/main/java/io/muenchendigital/digiwf/task/TaskVariables.java
@@ -1,2 +1,12 @@
-package io.muenchendigital.digiwf.task;public class TaskVariables {
+package io.muenchendigital.digiwf.task;
+
+import io.holunda.camunda.bpm.data.factory.VariableFactory;
+
+import static io.holunda.camunda.bpm.data.CamundaBpmData.stringVariable;
+
+/**
+ * Definition of task variables.
+ */
+public class TaskVariables {
+  public static final VariableFactory<String> APP_SCHEMA_REF = stringVariable("app_task_schema_key");
 }
