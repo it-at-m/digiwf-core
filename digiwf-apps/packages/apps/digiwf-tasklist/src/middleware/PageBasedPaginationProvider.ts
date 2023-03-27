a233b604f4ac4fd602fe0205e4c335f6b9754379
stephan.strehler <stephan.strehler@cgi.com>
2023-03-27 16:20:51 +0200

    #547: small refactoring in PageBasedPaginationProvider.ts

--- digiwf-apps/packages/apps/digiwf-tasklist/src/middleware/PageBasedPaginationProvider.ts
+++ digiwf-apps/packages/apps/digiwf-tasklist/src/middleware/PageBasedPaginationProvider.ts
@@ -5,43 +5,45 @@ export const DEFAULT_PAGE = 0;
 export const DEFAULT_SIZE = 20;
 
 export class PageBasedPaginationProvider {
-  private pageKeyToPaginationData: PageKeyToPaginationDataMap = new Map();
+  private pageKeyToPaginationData: Map<PageId, PaginationDataInSession> = new Map();
+
   public getPaginationDataInSession(pageId: PageId): PaginationDataInSession | undefined {
     return this.pageKeyToPaginationData.get(pageId)
   }
 
   public setPageOfPageId(pageId: PageId, page: number) {
-    const paginationInformationOfPage = this.pageKeyToPaginationData.get(pageId);
-    this.pageKeyToPaginationData?.set(pageId, {
-      size: paginationInformationOfPage?.size || DEFAULT_SIZE,
+    this.pageKeyToPaginationData.set(pageId, {
+      ...this.getPaginationDataInSessionOrDefault(pageId),
       page,
-      searchQuery: paginationInformationOfPage?.searchQuery
-    })
+    });
   }
 
-  public setSizeOfPageId = (pageId: PageId, size: number) => {
-    const paginationInformationOfPage = this.pageKeyToPaginationData.get(pageId);
+  public setSizeOfPageId(pageId: PageId, size: number) {
     this.pageKeyToPaginationData.set(pageId, {
+      ...this.getPaginationDataInSessionOrDefault(pageId),
       size,
-      page: paginationInformationOfPage?.page || DEFAULT_PAGE,
-      searchQuery: paginationInformationOfPage?.searchQuery
-    })
+    });
   }
-  public setSearchQuery = (pageId: PageId, searchQuery: string | undefined) => {
-    const paginationInformationOfPage = this.pageKeyToPaginationData.get(pageId);
-    this.pageKeyToPaginationData?.set(pageId, {
-      size: paginationInformationOfPage?.size || DEFAULT_SIZE,
-      page: paginationInformationOfPage?.page || DEFAULT_PAGE,
+
+  public setSearchQuery(pageId: PageId, searchQuery?: string) {
+    this.pageKeyToPaginationData.set(pageId, {
+      ...this.getPaginationDataInSessionOrDefault(pageId),
       searchQuery
     })
   }
-}
 
+  private getPaginationDataInSessionOrDefault(pageId: PageId): PaginationDataInSession {
+    const paginationInformationOfPage = this.pageKeyToPaginationData.get(pageId);
+    return {
+      size: paginationInformationOfPage?.size || DEFAULT_SIZE,
+      page: paginationInformationOfPage?.page || DEFAULT_PAGE,
+      searchQuery: paginationInformationOfPage?.searchQuery,
+    };
+  }
+}
 
 export interface PaginationDataInSession {
   readonly searchQuery?: string;
   readonly page: number;
   readonly size: number;
 }
-
-export type PageKeyToPaginationDataMap = Map<PageId, PaginationDataInSession>;
