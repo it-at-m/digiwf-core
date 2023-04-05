091fc519ad797226f50ebb9e093242471b909e51
Stephan Strehler <stephan.strehler@cgi.com>
2023-04-05 09:54:49 +0200

    #514: added session heartbeat interval for renewing the session

--- digiwf-apps/packages/apps/digiwf-tasklist/src/main.ts
+++ digiwf-apps/packages/apps/digiwf-tasklist/src/main.ts
@@ -16,9 +16,10 @@ import './plugins/digiwf-forms';
 import {VueQueryPlugin} from "@tanstack/vue-query";
 import {queryClient} from "./middleware/queryClient";
 import {initStatusCodeHandling} from "./api/statusCodeHandling";
+import {startSessionReloading} from "./api/sessionReloadHandler";
 
 initStatusCodeHandling();
-
+startSessionReloading();
 Vue.config.productionTip = false;
 
 Vue.use(VueQueryPlugin, {
