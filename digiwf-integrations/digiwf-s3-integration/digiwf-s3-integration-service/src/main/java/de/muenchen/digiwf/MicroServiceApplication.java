b7b42efa3e020838b3a5bfb498cdf5580b8ef891
Simon Hirtreiter <simon.hirtreiter@muenchen.de>
2023-04-06 06:46:15 +0200

    #59 init migrate digiwf-s3-integration-service from separate repo

---
+++ digiwf-integrations/digiwf-s3-integration/digiwf-s3-integration-service/src/main/java/de/muenchen/digiwf/MicroServiceApplication.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c): it@M - Dienstleister für Informations- und Telekommunikationstechnik
+ * der Landeshauptstadt München, 2022
+ */
+package de.muenchen.digiwf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
+/**
+ * Application class for starting the micro-service.
+ */
+@SpringBootApplication
+@EnableJpaAuditing
+@EnableScheduling
+public class MicroServiceApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroServiceApplication.class, args);
+    }
+
+
+}
