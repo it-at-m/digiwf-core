44be4b246e7ff807d11ab89cf6c85a6f7168e616
Christian Slawinger <ex.slawinger@muenchen.de>
2023-04-26 09:17:11 +0200

    UnitTest for IncidentNotifierHandler

---
+++ digiwf-engine/digiwf-engine-service/src/test/java/io/muenchendigital/digiwf/engine/incidents/IncidentNotifierHandlerTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c): it@M - Dienstleister für Informations- und Telekommunikationstechnik
+ * der Landeshauptstadt München, 2021
+ */
+package io.muenchendigital.digiwf.engine.incidents;
+
+import io.muenchendigital.digiwf.legacy.mailing.domain.model.MailTemplate;
+import io.muenchendigital.digiwf.legacy.mailing.domain.service.MailingService;
+import lombok.extern.slf4j.Slf4j;
+import org.camunda.bpm.engine.RepositoryService;
+import org.camunda.bpm.engine.impl.persistence.entity.IncidentEntity;
+import org.camunda.bpm.engine.repository.ProcessDefinition;
+import org.camunda.bpm.engine.repository.ProcessDefinitionQuery;
+import org.mockito.Mock;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
+
+/**
+ * Tests for IncidentNtifierHandler.
+ * 
+ * @author christian.slawinger
+ */
+@Slf4j
+@RunWith(MockitoJUnitRunner.class)
+public class IncidentNotifierHandlerTest {
+
+    @InjectMocks
+    private IncidentNotifierHandler incidentNotifierHandler;
+
+    @Mock
+    private RepositoryService repositoryService;
+
+    @Mock
+    private IncidentEntity incidentEntity;
+
+    @Mock
+    private ProcessDefinition processDefinition;
+
+    @Mock
+    private ProcessDefinitionQuery processDefinitionQuery;
+
+    @Mock
+    private MailingService mailingService;
+
+    /**
+     * Tests if no notification is send out when all notification switches are off. 
+     */
+    @Test
+    public void testHandleIncident() throws Exception {
+
+
+        Mockito.when(this.incidentEntity.getProcessDefinitionId()).thenReturn("Test123");
+        Mockito.when(this.processDefinition.getKey()).thenReturn("Testprozess");
+        Mockito.when(this.repositoryService.createProcessDefinitionQuery()).thenReturn(processDefinitionQuery);when(processDefinitionQuery.singleResult()).thenReturn(processDefinition);
+        Mockito.when(processDefinitionQuery.processDefinitionId(anyString())).thenReturn(processDefinitionQuery);
+
+        this.incidentNotifierHandler.sendInfoMail(incidentEntity);
+
+        ArgumentCaptor<MailTemplate> argument = ArgumentCaptor.forClass(MailTemplate.class);
+        verify(mailingService).sendMailTemplateWithLink(argument.capture());
+        assertTrue(argument.getValue().getBody().contains("In der Anwendung ist ein Incident aufgetreten (Prozessname: Testprozess)."));
+    }
+}
