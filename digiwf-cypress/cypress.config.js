2c808b9d4e1d75197bc253edfa5c52928cd9056d
angelina.hofmann <angelina.hofmann@muenchen.com>
2023-05-04 10:22:21 +0200

    set user as parameter

--- digiwf-cypress/cypress.config.js
+++ digiwf-cypress/cypress.config.js
@@ -1,6 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  env: {
+    username: "user",
+    password: "pw"
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- digiwf-cypress/cypress/e2e/was_ist_digiwf.cy.js
+++ digiwf-cypress/cypress/e2e/was_ist_digiwf.cy.js
@@ -1,8 +1,14 @@
 describe('template spec', () => {
     it('passes', () => {
+        const username = Cypress.env('username')
+        const password = Cypress.env('password')
+
+
         cy.visit('https://portal-digiwf-test.apps.capk.muenchen.de/')
-        cy.get("#username").type("");
-        cy.get("#password").type("");
+        cy.get("#username").type(username);
+        cy.get("#password").type(password);
         cy.get('[id^=kc-login]').click();
+        cy.get("#suchfeld").click();
+        cy.get("#suchfeld").type("DigiWftbhenjdtjshergrntujdtjhsrvgaebn");
     })
 })
