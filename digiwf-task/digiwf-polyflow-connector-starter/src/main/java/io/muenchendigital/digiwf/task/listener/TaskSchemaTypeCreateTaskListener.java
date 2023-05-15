c2366628ead0ff944d1da96e738cc7444cdf5f88
dominikhorn93 <dominik.horn93@gmail.com>
2023-05-15 17:38:26 +0200

    feat add schema key if vuetify form base

--- digiwf-task/digiwf-polyflow-connector-starter/src/main/java/io/muenchendigital/digiwf/task/listener/TaskSchemaTypeCreateTaskListener.java
+++ digiwf-task/digiwf-polyflow-connector-starter/src/main/java/io/muenchendigital/digiwf/task/listener/TaskSchemaTypeCreateTaskListener.java
@@ -1,7 +1,6 @@
 package io.muenchendigital.digiwf.task.listener;
 
 import io.holunda.polyflow.taskpool.collector.task.TaskEventCollectorService;
-import io.muenchendigital.digiwf.task.TaskSchemaType;
 import lombok.val;
 import org.camunda.bpm.engine.delegate.DelegateTask;
 import org.springframework.context.event.EventListener;
@@ -11,28 +10,29 @@ import org.springframework.util.StringUtils;
 
 import static io.holunda.camunda.bpm.data.CamundaBpmData.reader;
 import static io.holunda.camunda.bpm.data.CamundaBpmData.writer;
+import static io.muenchendigital.digiwf.task.TaskSchemaType.SCHEMA_BASED;
+import static io.muenchendigital.digiwf.task.TaskSchemaType.VUETIFY_FORM_BASE;
+import static io.muenchendigital.digiwf.task.TaskVariables.TASK_SCHEMA_KEY;
 import static io.muenchendigital.digiwf.task.TaskVariables.TASK_SCHEMA_TYPE;
 
 
 @Component
 public class TaskSchemaTypeCreateTaskListener {
 
-  @Order(TaskEventCollectorService.ORDER - 1000) // be before polyflow
-  @EventListener(condition = "#task.eventName.equals('create')")
-  public void taskCreated(final DelegateTask task) {
-    if (reader(task).getLocalOptional(TASK_SCHEMA_TYPE).isPresent()) {
-      return;
+    @Order(TaskEventCollectorService.ORDER - 1000) // be before polyflow
+    @EventListener(condition = "#task.eventName.equals('create')")
+    public void taskCreated(final DelegateTask task) {
+        if (reader(task).getLocalOptional(TASK_SCHEMA_TYPE).isPresent()) {
+            return;
+        }
+
+        val formKey = task.getProcessEngineServices().getFormService().getTaskFormKey(task.getProcessDefinitionId(), task.getTaskDefinitionKey());
+        val writer = writer(task);
+        if (StringUtils.hasText(formKey)) {
+            writer.setLocal(TASK_SCHEMA_TYPE, VUETIFY_FORM_BASE)
+                    .setLocal(TASK_SCHEMA_KEY, formKey);
+        } else {
+            writer.setLocal(TASK_SCHEMA_TYPE, SCHEMA_BASED);
+        }
     }
-
-    val formKey = task.getProcessEngineServices().getFormService().getTaskFormKey(task.getProcessDefinitionId(), task.getTaskDefinitionKey());
-
-    final TaskSchemaType taskType;
-    if (StringUtils.hasText(formKey)) {
-      taskType = TaskSchemaType.VUETIFY_FORM_BASE;
-    } else {
-      taskType = TaskSchemaType.SCHEMA_BASED;
-    }
-
-    writer(task).setLocal(TASK_SCHEMA_TYPE, taskType);
-  }
 }
--- digiwf-task/digiwf-polyflow-connector-starter/src/test/java/io/muenchendigital/digiwf/task/listener/TaskSchemaTypeCreateTaskListenerTest.java
+++ digiwf-task/digiwf-polyflow-connector-starter/src/test/java/io/muenchendigital/digiwf/task/listener/TaskSchemaTypeCreateTaskListenerTest.java
@@ -10,51 +10,50 @@ import org.junit.jupiter.api.Test;
 
 import java.util.UUID;
 
-import static io.holunda.camunda.bpm.data.CamundaBpmData.reader;
-import static io.muenchendigital.digiwf.task.TaskVariables.*;
+import static io.muenchendigital.digiwf.task.TaskVariables.TASK_SCHEMA_KEY;
+import static io.muenchendigital.digiwf.task.TaskVariables.TASK_SCHEMA_TYPE;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
-import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.task;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 class TaskSchemaTypeCreateTaskListenerTest {
 
-  private final TaskSchemaTypeCreateTaskListener listener = new TaskSchemaTypeCreateTaskListener();
-  private final ProcessEngineServices processEngineServices = mock(ProcessEngineServices.class);
-  private final FormService formService = mock(FormService.class);
+    private final TaskSchemaTypeCreateTaskListener listener = new TaskSchemaTypeCreateTaskListener();
+    private final ProcessEngineServices processEngineServices = mock(ProcessEngineServices.class);
+    private final FormService formService = mock(FormService.class);
 
-  @BeforeEach
-  public void setupMocks() {
-    when(processEngineServices.getFormService()).thenReturn(formService);
-  }
+    @BeforeEach
+    public void setupMocks() {
+        when(processEngineServices.getFormService()).thenReturn(formService);
+    }
 
-  @Test
-  public void detect_schema_based_form() {
-    val task = new DelegateTaskFake(UUID.randomUUID().toString())
-        .withProcessEngineServices(processEngineServices)
-        .withProcessDefinitionId(UUID.randomUUID().toString())
-        .withTaskDefinitionKey("task-key");
+    @Test
+    public void detect_schema_based_form() {
+        val task = new DelegateTaskFake(UUID.randomUUID().toString())
+                .withProcessEngineServices(processEngineServices)
+                .withProcessDefinitionId(UUID.randomUUID().toString())
+                .withTaskDefinitionKey("task-key");
 
-    when(formService.getTaskFormKey(any(), any())).thenReturn(null);
+        when(formService.getTaskFormKey(any(), any())).thenReturn(null);
 
-    listener.taskCreated(task);
-    assertThat(task.getVariablesLocal()).containsEntry(TASK_SCHEMA_TYPE.getName(), TaskSchemaType.SCHEMA_BASED);
-    verify(formService).getTaskFormKey(task.getProcessDefinitionId(), task.getTaskDefinitionKey());
-  }
+        listener.taskCreated(task);
+        assertThat(task.getVariablesLocal()).containsEntry(TASK_SCHEMA_TYPE.getName(), TaskSchemaType.SCHEMA_BASED);
+        verify(formService).getTaskFormKey(task.getProcessDefinitionId(), task.getTaskDefinitionKey());
+    }
 
-  @Test
-  public void detect_vuetify_form_base_form() {
-    val task = new DelegateTaskFake(UUID.randomUUID().toString())
-        .withProcessEngineServices(processEngineServices)
-        .withProcessDefinitionId(UUID.randomUUID().toString())
-        .withTaskDefinitionKey("task-key");
+    @Test
+    public void detect_vuetify_form_base_form() {
+        val task = new DelegateTaskFake(UUID.randomUUID().toString())
+                .withProcessEngineServices(processEngineServices)
+                .withProcessDefinitionId(UUID.randomUUID().toString())
+                .withTaskDefinitionKey("task-key");
 
-    when(formService.getTaskFormKey(any(), any())).thenReturn("form-ref");
+        when(formService.getTaskFormKey(any(), any())).thenReturn("form-ref");
 
-    listener.taskCreated(task);
-    assertThat(task.getVariablesLocal()).containsEntry(TASK_SCHEMA_TYPE.getName(), TaskSchemaType.VUETIFY_FORM_BASE);
-    verify(formService).getTaskFormKey(task.getProcessDefinitionId(), task.getTaskDefinitionKey());
-  }
+        listener.taskCreated(task);
+        assertThat(task.getVariablesLocal()).containsEntry(TASK_SCHEMA_TYPE.getName(), TaskSchemaType.VUETIFY_FORM_BASE);
+        assertThat(task.getVariablesLocal()).containsEntry(TASK_SCHEMA_KEY.getName(), "form-ref");
+        verify(formService).getTaskFormKey(task.getProcessDefinitionId(), task.getTaskDefinitionKey());
+    }
 
 }
