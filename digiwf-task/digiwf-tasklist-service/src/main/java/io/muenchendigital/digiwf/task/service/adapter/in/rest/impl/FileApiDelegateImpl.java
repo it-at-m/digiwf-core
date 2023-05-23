8d9ff959713a3ce357212665d5bc2ec20438800b
Julia Lehmann <julia.lehmann@muenchen.de>
2023-05-23 14:27:54 +0200

    Add FileApiDelegateImpl implementation

--- digiwf-task/digiwf-tasklist-service/src/main/java/io/muenchendigital/digiwf/task/service/adapter/in/rest/impl/FileApiDelegateImpl.java
+++ digiwf-task/digiwf-tasklist-service/src/main/java/io/muenchendigital/digiwf/task/service/adapter/in/rest/impl/FileApiDelegateImpl.java
@@ -1,8 +1,14 @@
 package io.muenchendigital.digiwf.task.service.adapter.in.rest.impl;
 
+import io.muenchendigital.digiwf.task.service.application.port.in.WorkOnTaskFile;
 import io.muenchendigital.digiwf.task.service.application.port.in.rest.api.FileApiDelegate;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+import static org.springframework.http.ResponseEntity.ok;
 
 /**
  * File API delegate for work on one user task files.
@@ -10,6 +16,18 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class FileApiDelegateImpl implements FileApiDelegate {
+
+    private final WorkOnTaskFile workOnTaskFile;
+
+    @Override
+    public ResponseEntity<List<String>> getFileNames(String taskId, String filePath) {
+        return ok(workOnTaskFile.getFileNames(taskId,filePath));
+    }
+
+    @Override
+    default ResponseEntity<String> getPresignedUrlForFile(String taskId, String fileName, String filePath, String requestMethod){
+        return ok(workOnTaskFile.getPresignedUrl(requestMethod, taskId, filePath, fileName));
+    }
 
 
 }
