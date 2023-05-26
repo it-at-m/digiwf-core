a249ed487471bacb2c88439a8e58da9cde9ade4d
Dominik Horn <dominik.horn93@gmail.com>
2023-05-26 11:23:12 +0200

    test: add filtering unit tests

--- digiwf-task/digiwf-tasklist-service/src/main/java/io/muenchendigital/digiwf/task/service/application/usecase/WorkOnUserTaskUseCase.java
+++ digiwf-task/digiwf-tasklist-service/src/main/java/io/muenchendigital/digiwf/task/service/application/usecase/WorkOnUserTaskUseCase.java
@@ -21,158 +21,157 @@ import org.springframework.stereotype.Component;
 import java.time.OffsetDateTime;
 import java.util.Map;
 
-import static org.aspectj.bridge.MessageUtil.fail;
-
 @Component
 @RequiredArgsConstructor
 public class WorkOnUserTaskUseCase implements WorkOnUserTask {
-  private final TaskQueryPort taskQueryPort;
-  private final CurrentUserPort currentUserPort;
-  private final TaskSchemaRefResolverPort taskSchemaRefResolverPort;
-  private final JsonSchemaPort jsonSchemaPort;
-  private final TaskCommandPort taskCommandPort;
-  private final LegacyPayloadTaskCommandPort legacyPayloadTasCommandPort;
-  private final LegacyFormValidationPort legacySchemaValidationPort;
-  private final JsonSchemaValidationPort jsonSchemaValidationPort;
-  private final CancellationFlagOutPort cancellationFlagOutPort;
-  private final TaskSchemaTypeResolverPort taskSchemaTypeResolverPort;
+    private final TaskQueryPort taskQueryPort;
+    private final CurrentUserPort currentUserPort;
+    private final TaskSchemaRefResolverPort taskSchemaRefResolverPort;
+    private final JsonSchemaPort jsonSchemaPort;
+    private final TaskCommandPort taskCommandPort;
+    private final LegacyPayloadTaskCommandPort legacyPayloadTasCommandPort;
+    private final LegacyFormValidationPort legacySchemaValidationPort;
+    private final JsonSchemaValidationPort jsonSchemaValidationPort;
+    private final CancellationFlagOutPort cancellationFlagOutPort;
+    private final TaskSchemaTypeResolverPort taskSchemaTypeResolverPort;
 
-  @Override
-  public JsonSchema loadSchema(String schemaId) throws JsonSchemaNotFoundException {
-    return jsonSchemaPort.getSchemaById(schemaId);
-  }
-
-  @Override
-  public TaskWithSchemaRef loadUserTask(String taskId) throws TaskNotFoundException {
-    val task = getTaskForUser(taskId);
-
-    val cancelable = cancellationFlagOutPort.apply(task);
-    val schemaRef = taskSchemaRefResolverPort.apply(task);
-    val type = taskSchemaTypeResolverPort.apply(task);
-
-    switch(type) {
-      case VUETIFY_FORM_BASE:
-        val form = legacyPayloadTasCommandPort.loadFormById(schemaRef);
-        this.filterLegacyFormBased(task, form);
-      case SCHEMA_BASED:
-        val schema = jsonSchemaPort.getSchemaById(schemaRef);
-        filterSchemaBased(task, schema);
-      default:
-        break;
+    @Override
+    public JsonSchema loadSchema(String schemaId) throws JsonSchemaNotFoundException {
+        return jsonSchemaPort.getSchemaById(schemaId);
     }
 
-    return new TaskWithSchemaRef(task, schemaRef, cancelable, type);
-  }
+    @Override
+    public TaskWithSchemaRef loadUserTask(String taskId) throws TaskNotFoundException {
+        val task = getTaskForUser(taskId);
 
-  @Override
-  public TaskWithSchema loadUserTaskWithSchema(String taskId) throws TaskNotFoundException, JsonSchemaNotFoundException {
-    val task = getTaskForUser(taskId);
-
-    val cancelable = cancellationFlagOutPort.apply(task);
-    val schemaRef = taskSchemaRefResolverPort.apply(task);
-    val type = taskSchemaTypeResolverPort.apply(task);
-    switch (type) {
-      case VUETIFY_FORM_BASE:
-        val form = legacyPayloadTasCommandPort.loadFormById(schemaRef);
-        this.filterLegacyFormBased(task, form);
-        return new TaskWithSchema(task, cancelable, type, null, form);
-      case SCHEMA_BASED:
-      default:
-        val schema = jsonSchemaPort.getSchemaById(schemaRef);
-        filterSchemaBased(task, schema);
-        return new TaskWithSchema(task, cancelable, type, schema, null);
-    }
-  }
-
-  @Override
-  public void completeUserTask(String taskId, Map<String, Object> payload) throws TaskNotFoundException {
-    val task = getTaskForUser(taskId);
-    switch (taskSchemaTypeResolverPort.apply(task)) {
-      case VUETIFY_FORM_BASE:
-        legacyPayloadTasCommandPort.completeOldSchemaUserTask(taskId, payload);
-        break;
-      case SCHEMA_BASED:
-      default:
+        val cancelable = cancellationFlagOutPort.apply(task);
         val schemaRef = taskSchemaRefResolverPort.apply(task);
-        val schema = jsonSchemaPort.getSchemaById(schemaRef);
-        val variables = jsonSchemaValidationPort.validateAndSerialize(schema, task, payload);
-        taskCommandPort.completeUserTask(taskId, variables);
-        break;
-    }
-  }
+        val type = taskSchemaTypeResolverPort.apply(task);
 
-  @Override
-  public void saveUserTask(String taskId, Map<String, Object> payload) throws TaskNotFoundException {
-    val task = getTaskForUser(taskId);
-    switch (taskSchemaTypeResolverPort.apply(task)) {
-      case VUETIFY_FORM_BASE:
-        legacyPayloadTasCommandPort.saveOldSchemaUserTask(taskId, payload);
-        break;
-      case SCHEMA_BASED:
-      default:
+        switch (type) {
+            case VUETIFY_FORM_BASE:
+                val form = legacyPayloadTasCommandPort.loadFormById(schemaRef);
+                this.filterLegacyFormBased(task, form);
+                break;
+            case SCHEMA_BASED:
+                val schema = jsonSchemaPort.getSchemaById(schemaRef);
+                this.filterSchemaBased(task, schema);
+            default:
+                break;
+        }
+
+        return new TaskWithSchemaRef(task, schemaRef, cancelable, type);
+    }
+
+    @Override
+    public TaskWithSchema loadUserTaskWithSchema(String taskId) throws TaskNotFoundException, JsonSchemaNotFoundException {
+        val task = getTaskForUser(taskId);
+
+        val cancelable = cancellationFlagOutPort.apply(task);
         val schemaRef = taskSchemaRefResolverPort.apply(task);
-        val schema = jsonSchemaPort.getSchemaById(schemaRef);
-        val variables = jsonSchemaValidationPort.validateAndSerialize(schema, task, payload);
-        taskCommandPort.saveUserTask(taskId, variables);
-        break;
+        val type = taskSchemaTypeResolverPort.apply(task);
+        switch (type) {
+            case VUETIFY_FORM_BASE:
+                val form = legacyPayloadTasCommandPort.loadFormById(schemaRef);
+                this.filterLegacyFormBased(task, form);
+                return new TaskWithSchema(task, cancelable, type, null, form);
+            case SCHEMA_BASED:
+            default:
+                val schema = jsonSchemaPort.getSchemaById(schemaRef);
+                filterSchemaBased(task, schema);
+                return new TaskWithSchema(task, cancelable, type, schema, null);
+        }
     }
-  }
 
-  @Override
-  public void assignUserTask(String taskId, String assignee) throws TaskNotFoundException {
-    val task = getTaskForUser(taskId);
-    if (!assignee.equals(task.getAssignee())) {
-      taskCommandPort.assignUserTask(taskId, assignee);
+    @Override
+    public void completeUserTask(String taskId, Map<String, Object> payload) throws TaskNotFoundException {
+        val task = getTaskForUser(taskId);
+        switch (taskSchemaTypeResolverPort.apply(task)) {
+            case VUETIFY_FORM_BASE:
+                legacyPayloadTasCommandPort.completeOldSchemaUserTask(taskId, payload);
+                break;
+            case SCHEMA_BASED:
+            default:
+                val schemaRef = taskSchemaRefResolverPort.apply(task);
+                val schema = jsonSchemaPort.getSchemaById(schemaRef);
+                val variables = jsonSchemaValidationPort.validateAndSerialize(schema, task, payload);
+                taskCommandPort.completeUserTask(taskId, variables);
+                break;
+        }
     }
-  }
 
-  @Override
-  public void unassignUserTask(String taskId) throws TaskNotFoundException {
-    val task = getTaskForUser(taskId);
-    if (task.getAssignee() != null) {
-      taskCommandPort.unassignUserTask(taskId);
+    @Override
+    public void saveUserTask(String taskId, Map<String, Object> payload) throws TaskNotFoundException {
+        val task = getTaskForUser(taskId);
+        switch (taskSchemaTypeResolverPort.apply(task)) {
+            case VUETIFY_FORM_BASE:
+                legacyPayloadTasCommandPort.saveOldSchemaUserTask(taskId, payload);
+                break;
+            case SCHEMA_BASED:
+            default:
+                val schemaRef = taskSchemaRefResolverPort.apply(task);
+                val schema = jsonSchemaPort.getSchemaById(schemaRef);
+                val variables = jsonSchemaValidationPort.validateAndSerialize(schema, task, payload);
+                taskCommandPort.saveUserTask(taskId, variables);
+                break;
+        }
     }
-  }
 
-  @Override
-  public void deferUserTask(String taskId, OffsetDateTime followUpDate) throws TaskNotFoundException {
-    val task = getTaskForUser(taskId);
-    taskCommandPort.deferUserTask(taskId, followUpDate.toInstant());
-  }
-
-  @Override
-  public void undeferUserTask(String taskId) throws TaskNotFoundException {
-    val task = getTaskForUser(taskId);
-    if (task.getFollowUpDate() != null) {
-      taskCommandPort.undeferUserTask(taskId);
+    @Override
+    public void assignUserTask(String taskId, String assignee) throws TaskNotFoundException {
+        val task = getTaskForUser(taskId);
+        if (!assignee.equals(task.getAssignee())) {
+            taskCommandPort.assignUserTask(taskId, assignee);
+        }
     }
-  }
 
-  @Override
-  public void cancelUserTask(String taskId) throws TaskNotFoundException {
-    val task = getTaskForUser(taskId);
-    if (cancellationFlagOutPort.apply(task)) {
-      taskCommandPort.cancelUserTask(taskId);
-    } else {
-      throw new IllegalArgumentException("Task " + taskId + " can not be cancelled.");
+    @Override
+    public void unassignUserTask(String taskId) throws TaskNotFoundException {
+        val task = getTaskForUser(taskId);
+        if (task.getAssignee() != null) {
+            taskCommandPort.unassignUserTask(taskId);
+        }
     }
-  }
 
-  private Task getTaskForUser(String taskId) {
-    val currentUser = currentUserPort.getCurrentUser();
-    return taskQueryPort.getTaskByIdForCurrentUser(currentUser, taskId);
-  }
+    @Override
+    public void deferUserTask(String taskId, OffsetDateTime followUpDate) throws TaskNotFoundException {
+        val task = getTaskForUser(taskId);
+        taskCommandPort.deferUserTask(taskId, followUpDate.toInstant());
+    }
 
-  private void filterSchemaBased(Task task, JsonSchema schema) {
-    val filteredPayload =  this.jsonSchemaValidationPort.filterVariables(task.getPayload(), schema);
-    task.getPayload().clear();
-    task.getPayload().putAll(filteredPayload);
-  }
+    @Override
+    public void undeferUserTask(String taskId) throws TaskNotFoundException {
+        val task = getTaskForUser(taskId);
+        if (task.getFollowUpDate() != null) {
+            taskCommandPort.undeferUserTask(taskId);
+        }
+    }
 
-  private void filterLegacyFormBased(Task task, Form form) {
-    val filteredPayload = legacySchemaValidationPort.filterVariables(task.getPayload(), form);
-    task.getPayload().clear();
-    task.getPayload().putAll(filteredPayload);
-  }
+    @Override
+    public void cancelUserTask(String taskId) throws TaskNotFoundException {
+        val task = getTaskForUser(taskId);
+        if (cancellationFlagOutPort.apply(task)) {
+            taskCommandPort.cancelUserTask(taskId);
+        } else {
+            throw new IllegalArgumentException("Task " + taskId + " can not be cancelled.");
+        }
+    }
+
+    private Task getTaskForUser(String taskId) {
+        val currentUser = currentUserPort.getCurrentUser();
+        return taskQueryPort.getTaskByIdForCurrentUser(currentUser, taskId);
+    }
+
+    private void filterSchemaBased(Task task, JsonSchema schema) {
+        val filteredPayload = this.jsonSchemaValidationPort.filterVariables(task.getPayload(), schema);
+        task.getPayload().clear();
+        task.getPayload().putAll(filteredPayload);
+    }
+
+    private void filterLegacyFormBased(Task task, Form form) {
+        val filteredPayload = legacySchemaValidationPort.filterVariables(task.getPayload(), form);
+        task.getPayload().clear();
+        task.getPayload().putAll(filteredPayload);
+    }
 
 }
