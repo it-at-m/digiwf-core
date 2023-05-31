228d1b38a91469ff3c4cf2c067173edf23ccfef1
Lukas Mösle <lukas95m@yahoo.de>
2023-05-31 18:15:58 +0200

    feature(#387): add camunda cockpit

---
+++ digiwf-engine/digiwf-engine-cockpit/README.md
@@ -0,0 +1,3 @@
+# Camunda Cockpit
+
+Run this application, visit [http://localhost:8084](http://localhost:8084) and login with admin:admin.
---
+++ digiwf-engine/digiwf-engine-cockpit/src/main/java/io/muenchendigital/digiwf/cockpit/CamundaCockpitApplication.java
@@ -0,0 +1,14 @@
+package io.muenchendigital.digiwf.cockpit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+public class CamundaCockpitApplication {
+
+  public static void main(String... args) {
+    SpringApplication.run(CamundaCockpitApplication.class, args);
+  }
+}
