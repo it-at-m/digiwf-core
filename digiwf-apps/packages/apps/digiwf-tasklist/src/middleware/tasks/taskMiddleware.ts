0844a781b4ffa04ae8c772a69e803bbe6c8865d9
stephan.strehler <stephan.strehler@cgi.com>
2023-06-02 11:03:42 +0200

    #401: fix wrong feature toggle logic

--- digiwf-apps/packages/apps/digiwf-tasklist/src/middleware/tasks/taskMiddleware.ts
+++ digiwf-apps/packages/apps/digiwf-tasklist/src/middleware/tasks/taskMiddleware.ts
@@ -392,8 +392,8 @@ export const assignTask = (taskId: string,): Promise<AssignTaskResult> => {
   const userId = store.getters["user/info"].lhmObjectId;
   return (
     shouldUseTaskService()
-      ? callAssignTaskInEngine(taskId)
-      : callAssignTaskInTaskService(taskId, userId)
+      ? callAssignTaskInTaskService(taskId, userId)
+      : callAssignTaskInEngine(taskId)
   ).then(() => {
     router.push({path: "/task/" + taskId});
     queryClient.invalidateQueries([userTasksQueryId])
