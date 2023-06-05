c65717f02713f5fed9484085dbd54bdc31c0ba5c
stephan.strehler <stephan.strehler@cgi.com>
2023-06-05 17:15:48 +0200

    #391: update feature toggle logic

--- digiwf-apps/packages/apps/digiwf-tasklist/src/utils/featureToggles.ts
+++ digiwf-apps/packages/apps/digiwf-tasklist/src/utils/featureToggles.ts
@@ -4,21 +4,35 @@ const COOKIE_SHOW_BETA_BUTTON_KEY = "FEATURE_SHOW_BETA_BUTTON";
 
 /**
  * if FEATURE_USE_TASK_SERVICE cookie is set to true, the taskservice will allways used.
- * Otherwise when the FEATURE_SHOW_BETA_BUTTON cookie is set to true and the users activated the feature, the taskservice
+ * Otherwise when the FEATURE_SHOW_BETA_BUTTON cookie is set to true and the users activated the feature,
+ * the taskservice is used when the current localStorage value is true
  */
-export const shouldUseTaskService = (): boolean => {
+export const initShouldUseTaskServiceFeatureToggle = () => {
   const useTaskServiceCookieValue: boolean = getCookie(COOKIE_USE_TASKSERVICE_KEY).trim().toLowerCase() === "true";
   const showBetaButtonCookieValue: boolean = getCookie(COOKIE_SHOW_BETA_BUTTON_KEY).trim().toLowerCase() === "true";
-  const isTaskServiceActivatedInLocalStorage = localStorage.getItem(LOCAL_STORAGE_USE_TASKSERVICE_KEY)?.toLocaleLowerCase().trim() === "true";
+  const isTaskServiceActivatedInLocalStorage = localStorage.getItem(LOCAL_STORAGE_USE_TASKSERVICE_KEY);
 
-  if (useTaskServiceCookieValue && isTaskServiceActivatedInLocalStorage) {
-    return true;
+  if(useTaskServiceCookieValue) {
+    setShouldUseTaskService(true);
   }
-  return showBetaButtonCookieValue && isTaskServiceActivatedInLocalStorage;
+  if(isTaskServiceActivatedInLocalStorage === null) {
+    setShouldUseTaskService(useTaskServiceCookieValue);
+    return
+  }
+  if(!showBetaButtonCookieValue) { // if the user has not the permission to change
+    setShouldUseTaskService(useTaskServiceCookieValue);
+  }
+}
+export const shouldUseTaskService = (): boolean => {
+  return localStorage.getItem(LOCAL_STORAGE_USE_TASKSERVICE_KEY)?.trim().toLowerCase() === "true"
 }
 
 export const setShouldUseTaskService = (newValue: boolean) => {
   localStorage.setItem(LOCAL_STORAGE_USE_TASKSERVICE_KEY, newValue ? "true" : "false")
+}
+
+export const switchShouldUseTaskService = () => {
+  setShouldUseTaskService(!shouldUseTaskService());
 }
 
 export const shouldShowBetaButton = (): boolean => {
