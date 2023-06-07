24adf90f92695645a9e2f21497656f98a5f38236
Julia Lehmann <julia.lehmann@muenchen.de>
2023-06-07 14:06:42 +0200

    #165: add digiwf-message lib and adapt MessageProcessor

--- digiwf-integrations/digiwf-cosys-integration/digiwf-cosys-integration-core/src/main/java/io/muenchendigital/digiwf/cosys/integration/api/MessageProcessor.java
+++ digiwf-integrations/digiwf-cosys-integration/digiwf-cosys-integration-core/src/main/java/io/muenchendigital/digiwf/cosys/integration/api/MessageProcessor.java
@@ -2,18 +2,20 @@ package io.muenchendigital.digiwf.cosys.integration.api;
 
 import io.muenchendigital.digiwf.cosys.integration.domain.model.GenerateDocument;
 import io.muenchendigital.digiwf.cosys.integration.domain.service.CosysService;
-import io.muenchendigital.digiwf.spring.cloudstream.utils.api.streaming.incident.service.IncidentService;
-import io.muenchendigital.digiwf.spring.cloudstream.utils.api.streaming.message.service.CorrelateMessageService;
+import io.muenchendigital.digiwf.message.process.api.ProcessApi;
+import io.muenchendigital.digiwf.message.process.api.ErrorApi;
+import io.muenchendigital.digiwf.message.process.api.error.BpmnError;
+import io.muenchendigital.digiwf.message.process.api.error.IncidentError;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageHeaders;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Consumer;
+
+import static io.muenchendigital.digiwf.message.common.MessageConstants.DIGIWF_PROCESS_INSTANCE_ID;
 
 @Slf4j
 @Component
@@ -21,9 +23,10 @@ import java.util.function.Consumer;
 public class MessageProcessor {
 
     private final CosysService cosysService;
-    private final CorrelateMessageService correlateMessageService;
-    private final IncidentService incidentService;
 
+    private final ProcessApi processApi;
+
+    private final ErrorApi errorApi;
 
     /**
      * All messages from the route "generateDocument" go here.
@@ -33,35 +36,22 @@ public class MessageProcessor {
     @Bean
     public Consumer<Message<GenerateDocument>> createCosysDocument() {
         return message -> {
+            try {
             log.info("Processing generate document request from eventbus");
             final GenerateDocument document = message.getPayload();
             log.debug("Generate document request: {}", document);
-            try {
                 this.cosysService.createDocument(document);
-                this.emitResponse(message.getHeaders());
-            } catch (final Exception err) {
-                log.error("Document could not be created: {}", err.getMessage());
-                if (!emitIncident(message.getHeaders(), "Document could not be created: " + err.getMessage())){
-                    log.error("Emitting registration error failed");
-                }
-
-
+                this.correlateMessage((message.getHeaders().get(DIGIWF_PROCESS_INSTANCE_ID).toString()), "documentCreated", Map.of("status", true));
+            } catch (final BpmnError bpmnError) {
+                this.errorApi.handleBpmnError(message.getHeaders(), bpmnError);
+            } catch (final IncidentError incidentError) {
+                this.errorApi.handleIncident(message.getHeaders(), incidentError);
             }
         };
     }
 
-    /**
-     * Function to emit a response using the correlateMessageService of digiwf-spring-cloudstream-utils
-     *
-     * @param messageHeaders The MessageHeaders of the incoming message you want to correlate your answer to
-     */
-    private void emitResponse(final MessageHeaders messageHeaders) {
-        final Map<String, Object> correlatePayload = new HashMap<>();
-        correlatePayload.put("status", true);
-        this.correlateMessageService.sendCorrelateMessage(messageHeaders, correlatePayload);
+    public void correlateMessage(final String processInstanceId, final String messageName, final Map<String, Object> message) {
+        this.processApi.correlateMessage(processInstanceId, messageName, message);
     }
 
-    private boolean emitIncident(final MessageHeaders messageHeaders, final String errorMessage) {
-        return this.incidentService.sendIncident(messageHeaders, errorMessage);
-    }
 }
