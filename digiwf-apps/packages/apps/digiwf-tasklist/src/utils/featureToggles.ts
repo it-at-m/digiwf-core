84926182bbe0899f1672ce3f405ebde6ee019ee1
Lukas Mösle <lukas95m@yahoo.de>
2023-06-07 09:38:20 +0200

    feature(#425): simplify feature toggle logic

--- digiwf-apps/packages/apps/digiwf-tasklist/src/utils/featureToggles.ts
+++ digiwf-apps/packages/apps/digiwf-tasklist/src/utils/featureToggles.ts
@@ -10,15 +10,12 @@ const COOKIE_SHOW_BETA_BUTTON_KEY = "FEATURE_SHOW_BETA_BUTTON";
 export const initShouldUseTaskServiceFeatureToggle = () => {
   const useTaskServiceCookieValue: boolean = getCookie(COOKIE_USE_TASKSERVICE_KEY).trim().toLowerCase() === "true";
   const showBetaButtonCookieValue: boolean = getCookie(COOKIE_SHOW_BETA_BUTTON_KEY).trim().toLowerCase() === "true";
-  const isTaskServiceActivatedInLocalStorage = localStorage.getItem(LOCAL_STORAGE_USE_TASKSERVICE_KEY);
 
-  if(useTaskServiceCookieValue) {
-    setShouldUseTaskService(true);
-  }
-  if(isTaskServiceActivatedInLocalStorage === null) {
+  // if cookie and localStorage values are different, cookie wins
+  if (showBetaButtonCookieValue !== shouldUseTaskService()) {
     setShouldUseTaskService(useTaskServiceCookieValue);
-    return
   }
+
   if(!showBetaButtonCookieValue) { // if the user has not the permission to change
     setShouldUseTaskService(useTaskServiceCookieValue);
   }
