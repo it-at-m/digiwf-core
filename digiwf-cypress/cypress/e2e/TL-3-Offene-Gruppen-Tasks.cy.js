48f6661b0e22669be169e8ae09a9701522078d00
Angelina Hofmann <angelina.hofmann@muenchen.de>
2023-08-11 14:09:24 +0200

    init start grouptask

--- digiwf-cypress/cypress/e2e/TL-3-Offene-Gruppen-Tasks.cy.js
+++ digiwf-cypress/cypress/e2e/TL-3-Offene-Gruppen-Tasks.cy.js
@@ -1,3 +1,11 @@
+import vorgangStarten from "../pages/vorgangStarten"
+import meineAufgaben from "../pages/meineAufgaben"
+import * as dataElementKeys from "../constants/dataElementKeys"
+import * as environmentVariables from "../constants/environmentVariables"
+import exampleGroupTask from "../pages/exampleGroupTask";
+import exampleUserTask from "../pages/exampleUserTask";
+
+const numberOfTasks = 2
 
 before(() => {
     cy.login()
@@ -6,6 +14,16 @@ before(() => {
 describe('offene Gruppentasks anzeigen', () => {
     it('Gruppen Task Ansicht testen', () => {
 
+        // Step 0: Create group tasks
+        cy.log('Step 0');
+        meineAufgaben.openVorgangStarten();
+        vorgangStarten.findProcess(dataElementKeys.EXAMPLE_GROUP_TASK_NAME);
+        vorgangStarten.clickListElement(dataElementKeys.EXAMPLE_GROUP_TASK_KEY);
+        exampleUserTask.setNumberOfTasks(numberOfTasks);
+        exampleGroupTask.setGroup("group1");
+        cy.log("abschliessen")
+        exampleGroupTask.clickAbschliessen();
+        cy.log("after")
     });
 
 })
---
+++ digiwf-cypress/cypress/pages/exampleGroupTask.js
@@ -0,0 +1,30 @@
+import Page from './page'
+
+class ExampleGroupTask extends Page{
+    elements = {
+        numberOfParallelTasks: () => cy.get("#allOf-0-allOf-0-FORMFIELD_NumberOfTasks"),
+        groupForTask: () => cy.get(`div.col-sm-12:nth-child(3) > div:nth-child(1)`),
+        abschliessenButton: () => cy.get(`button.mt-5`)
+    }
+
+    setGroup(group){
+        this.elements.groupForTask().type(group)
+    }
+
+    setNumberOfTasks(number){
+        this.elements.numberOfParallelTasks().clear()
+        this.elements.numberOfParallelTasks().type(number)
+    }
+
+    clickAbschliessen(){
+        cy.intercept({
+            method: 'GET',
+            url: '/api/digitalwf-backend-service/rest/filter',
+        }).as('dataGetFilter')
+        this.elements.abschliessenButton().click()
+        cy.wait('@dataGetFilter',{ timeout: 50000 }).its('response.statusCode').should('equal', 200)
+    }
+}
+
+
+module.exports = new ExampleGroupTask();
