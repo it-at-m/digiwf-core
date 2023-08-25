27e5327cbf5a4caf55d157ee7a9010e1279abdaf
stephan.strehler <stephan.strehler@cgi.com>
2023-08-25 13:13:41 +0200

    #609: update unit tests

---
+++ digiwf-engine/digiwf-engine-service/src/test/java/de/muenchen/oss/digiwf/process/ProcessMock.java
@@ -0,0 +1,16 @@
+package de.muenchen.oss.digiwf.process;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class ProcessMock implements Serializable {
+
+    private String description;
+
+}
