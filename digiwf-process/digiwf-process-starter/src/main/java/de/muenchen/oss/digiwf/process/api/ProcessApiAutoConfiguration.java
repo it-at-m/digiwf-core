4c61bf92f14a85c56c8341ddddcfa6ed9cbc1cf1
Julia Lehmann <julia.lehmann@muenchen.de>
2023-09-19 16:35:25 +0200

    #639: ProcessConfigConstants

--- digiwf-process/digiwf-process-starter/src/main/java/de/muenchen/oss/digiwf/process/api/ProcessApiAutoConfiguration.java
+++ digiwf-process/digiwf-process-starter/src/main/java/de/muenchen/oss/digiwf/process/api/ProcessApiAutoConfiguration.java
@@ -4,8 +4,12 @@ import de.muenchen.oss.digiwf.process.api.config.api.ProcessConfigApi;
 import de.muenchen.oss.digiwf.process.api.config.impl.ProcessConfigApiImpl;
 import de.muenchen.oss.digiwf.process.api.config.impl.ProcessConfigClient;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableFeignClients
 public class ProcessApiAutoConfiguration {
 
     @Bean
