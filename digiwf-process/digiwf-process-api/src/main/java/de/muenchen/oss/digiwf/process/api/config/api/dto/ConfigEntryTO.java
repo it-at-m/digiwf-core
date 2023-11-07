997b60f21919c17537317b127d36efe3e09e66d0
stephan.strehler <stephan.strehler@cgi.com>
2023-11-07 19:35:00 +0100

    #768: code fixed in different places

--- digiwf-process/digiwf-process-api/src/main/java/de/muenchen/oss/digiwf/process/api/config/api/dto/ConfigEntryTO.java
+++ digiwf-process/digiwf-process-api/src/main/java/de/muenchen/oss/digiwf/process/api/config/api/dto/ConfigEntryTO.java
@@ -1,11 +1,10 @@
 package de.muenchen.oss.digiwf.process.api.config.api.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.validation.constraints.NotBlank;
 
 
 @Data
