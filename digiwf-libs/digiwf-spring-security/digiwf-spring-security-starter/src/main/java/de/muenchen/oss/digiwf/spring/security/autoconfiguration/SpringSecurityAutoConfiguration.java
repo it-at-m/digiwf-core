8a23f4185098c5ef0f269878c6ff98e900b91ba2
Simon Zambrovski <simon.zambrovski@holisticon.de>
2023-11-09 11:14:49 +0100

    organize imports

--- digiwf-libs/digiwf-spring-security/digiwf-spring-security-starter/src/main/java/de/muenchen/oss/digiwf/spring/security/autoconfiguration/SpringSecurityAutoConfiguration.java
+++ digiwf-libs/digiwf-spring-security/digiwf-spring-security-starter/src/main/java/de/muenchen/oss/digiwf/spring/security/autoconfiguration/SpringSecurityAutoConfiguration.java
@@ -3,7 +3,6 @@ package de.muenchen.oss.digiwf.spring.security.autoconfiguration;
 
 import de.muenchen.oss.digiwf.spring.security.SpringSecurityProperties;
 import de.muenchen.oss.digiwf.spring.security.factory.YamlPropertySourceFactory;
-import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
