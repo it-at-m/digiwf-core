1d7ba9b10ab604d9265482dec08ce120f332bd8f
Julia Lehmann <julia.lehmann@muenchen.de>
2023-11-13 16:01:41 +0100

    #764: delete @EnableFeignClients

--- digiwf-process/digiwf-process-example/src/main/java/de/muenchen/oss/digiwf/DigiwfProcessExample.java
+++ digiwf-process/digiwf-process-example/src/main/java/de/muenchen/oss/digiwf/DigiwfProcessExample.java
@@ -2,9 +2,7 @@ package de.muenchen.oss.digiwf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
 @SpringBootApplication
 public class DigiwfProcessExample {
 
