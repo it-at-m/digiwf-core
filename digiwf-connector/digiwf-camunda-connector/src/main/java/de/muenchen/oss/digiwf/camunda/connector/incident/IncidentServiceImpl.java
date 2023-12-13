b1f909e5bb0ba308fa2359ce28aff738c2038cdf
Lukas Mösle <lukas95m@yahoo.de>
2023-12-13 17:19:21 +0100

    refactor: use camunda-platform-7-rest-client-spring-boot in connector

--- digiwf-connector/digiwf-camunda-connector/src/main/java/de/muenchen/oss/digiwf/camunda/connector/incident/IncidentServiceImpl.java
+++ digiwf-connector/digiwf-camunda-connector/src/main/java/de/muenchen/oss/digiwf/camunda/connector/incident/IncidentServiceImpl.java
@@ -6,8 +6,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.camunda.community.rest.client.api.EventSubscriptionApi;
 import org.camunda.community.rest.client.api.ExecutionApi;
+import org.camunda.community.rest.client.model.CreateIncidentDto;
+import org.camunda.community.rest.client.model.EventSubscriptionDto;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
+
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @Slf4j
 @Service
@@ -22,51 +27,51 @@ public class IncidentServiceImpl implements IncidentService {
 
     @Override
     public void createIncident(final String processInstanceId, final String messageName, final String messageContent) {
-//        try {
+        try {
 
             //check parameters
             Assert.notNull(processInstanceId, "process instance id cannot be empty");
             Assert.notNull(messageName, "message name cannot be empty");
 
             //load corresponding event subscription
-//            final String executionId = this.eventSubscriptionApi.getEventSubscriptions(
-//                    null,
-//                    messageName,
-//                    EVENT_TYPE,
-//                    null,
-//                    processInstanceId,
-//                    null,
-//                    null,
-//                    null,
-//                    null,
-//                    null,
-//                    null,
-//                    null,
-//                    null)
-//                    .stream()
-//                    .findFirst()
-//                    .map(EventSubscriptionDto::getExecutionId)
-//                    .orElseThrow();
-//
-//            // create incident body
-//            final CreateIncidentDto createIncidentDto = new CreateIncidentDto();
-//            createIncidentDto.setIncidentType(INCIDENT_TYPE);
-//            createIncidentDto.setMessage(
-//                    messageContent != null  && !messageContent.isBlank()
-//                        ? messageContent
-//                        : "Error occurred in integration service"
-//            );
-//
-//            // send create incident call
-//            this.executionApi.createIncident(executionId, createIncidentDto);
-//
-//        } catch (final ApiException e) {
-//            log.error("Cannot create incident for processinstance id {} and message name {}: {}", processInstanceId, messageName, e.getResponseBody());
-//            throw new RuntimeException(e);
-//        } catch (final NoSuchElementException | IllegalArgumentException e) {
-//            log.error("Cannot create incident for processinstance id {} and message name {}", processInstanceId, messageName);
-//            throw new RuntimeException(e);
-//        }
+            final List<EventSubscriptionDto> eventSubscriptions = this.eventSubscriptionApi.getEventSubscriptions(
+                            null,
+                            messageName,
+                            EVENT_TYPE,
+                            null,
+                            processInstanceId,
+                            null,
+                            null,
+                            null,
+                            null,
+                            null,
+                            null,
+                            null,
+                            null)
+                    .getBody();
+            if (eventSubscriptions.isEmpty()) {
+                throw new NoSuchElementException();
+            }
+            final String executionId = eventSubscriptions.stream()
+                    .findFirst()
+                    .map(EventSubscriptionDto::getExecutionId)
+                    .orElseThrow();
+
+            // create incident body
+            final CreateIncidentDto createIncidentDto = new CreateIncidentDto();
+            createIncidentDto.setIncidentType(INCIDENT_TYPE);
+            createIncidentDto.setMessage(
+                    messageContent != null  && !messageContent.isBlank()
+                        ? messageContent
+                        : "Error occurred in integration service"
+            );
+
+            // send create incident call
+            this.executionApi.createIncident(executionId, createIncidentDto);
+        } catch (final NoSuchElementException | IllegalArgumentException e) {
+            log.error("Cannot create incident for processinstance id {} and message name {}", processInstanceId, messageName);
+            throw new RuntimeException(e);
+        }
 
     }
 }
