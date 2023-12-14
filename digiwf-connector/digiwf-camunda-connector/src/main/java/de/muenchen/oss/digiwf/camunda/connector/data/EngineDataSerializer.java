2556d4eb3f04331fa0c139368404e2290a2755d7
Lukas Mösle <lukas95m@yahoo.de>
2023-12-14 10:41:53 +0100

    refactor: use message api again

--- digiwf-connector/digiwf-camunda-connector/src/main/java/de/muenchen/oss/digiwf/camunda/connector/data/EngineDataSerializer.java
+++ digiwf-connector/digiwf-camunda-connector/src/main/java/de/muenchen/oss/digiwf/camunda/connector/data/EngineDataSerializer.java
@@ -2,6 +2,7 @@ package de.muenchen.oss.digiwf.camunda.connector.data;
 
 import org.camunda.bpm.engine.variable.VariableMap;
 import org.camunda.bpm.engine.variable.value.TypedValue;
+import org.camunda.community.rest.client.model.VariableValueDto;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.stereotype.Component;
@@ -12,6 +13,17 @@ import java.util.Map;
 
 @Component
 public class EngineDataSerializer {
+
+    public Map<String, VariableValueDto> toEngineData(final Map<String, Object> data) {
+        final JSONObject jsonData = new JSONObject(data);
+        final Map<String, VariableValueDto> variables = new HashMap<>();
+        jsonData.keySet().forEach(key -> {
+                    final Object value = jsonData.get(key);
+                    variables.put(key, this.toEngineData(value));
+                }
+        );
+        return variables;
+    }
 
     public Map<String, Object> fromEngineData(final VariableMap variables) {
         final Map<String, Object> data = new HashMap<>();
@@ -27,6 +39,17 @@ public class EngineDataSerializer {
         return data;
     }
 
+    public VariableValueDto toEngineData(final Object value) {
+        final VariableValueDto variableValueDto = new VariableValueDto();
+        if (value instanceof JSONObject || value instanceof JSONArray) {
+            variableValueDto.setValue(value.toString());
+            variableValueDto.setType("json");
+        } else {
+            variableValueDto.setValue(value);
+        }
+        return variableValueDto;
+    }
+
     //---------------------------------- helper methods ----------------------------------//
 
     private Object fromEngineData(final Object value) {
@@ -35,6 +58,4 @@ public class EngineDataSerializer {
         }
         return new JSONObject(value.toString()).toMap();
     }
-
-
 }
--- digiwf-connector/digiwf-camunda-connector/src/main/java/de/muenchen/oss/digiwf/camunda/connector/message/MessageServiceImpl.java
+++ digiwf-connector/digiwf-camunda-connector/src/main/java/de/muenchen/oss/digiwf/camunda/connector/message/MessageServiceImpl.java
@@ -1,43 +1,53 @@
 package de.muenchen.oss.digiwf.camunda.connector.message;
 
 
+import de.muenchen.oss.digiwf.camunda.connector.data.EngineDataSerializer;
 import de.muenchen.oss.digiwf.connector.api.message.CorrelateMessage;
 import de.muenchen.oss.digiwf.connector.api.message.MessageService;
 import io.micrometer.common.util.StringUtils;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.camunda.bpm.engine.RuntimeService;
-import org.camunda.bpm.engine.runtime.MessageCorrelationBuilder;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.camunda.community.rest.client.api.MessageApi;
+import org.camunda.community.rest.client.model.CorrelationMessageDto;
+import org.camunda.community.rest.client.model.VariableValueDto;
+import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
 @Slf4j
+@Service
+@RequiredArgsConstructor
 public class MessageServiceImpl implements MessageService {
 
-    private final RuntimeService runtimeService;
-
-    public MessageServiceImpl(@Qualifier("remote") final RuntimeService runtimeService) {
-        this.runtimeService = runtimeService;
-    }
+    private final MessageApi messageApi;
+    private final EngineDataSerializer serializer;
 
     @Override
     public void correlateMessage(final CorrelateMessage correlateMessage) {
         log.debug("correlateMessage {}", correlateMessage);
 
-        MessageCorrelationBuilder messageCorrelationBuilder = runtimeService.createMessageCorrelation(correlateMessage.getMessageName());
+        final CorrelationMessageDto correlationMessageDto = new CorrelationMessageDto();
+        correlationMessageDto.setMessageName(correlateMessage.getMessageName());
+
+        if (correlateMessage.getPayloadVariables() != null && !correlateMessage.getPayloadVariables().isEmpty()) {
+            final Map<String, VariableValueDto> variables = this.serializer.toEngineData(correlateMessage.getPayloadVariables());
+            correlationMessageDto.setProcessVariables(variables);
+        }
+
+        if (correlateMessage.getPayloadVariablesLocal() != null && !correlateMessage.getPayloadVariablesLocal().isEmpty()) {
+            final Map<String, VariableValueDto> variables = this.serializer.toEngineData(correlateMessage.getPayloadVariablesLocal());
+            correlationMessageDto.setProcessVariablesLocal(variables);
+        }
 
         if (StringUtils.isNotBlank(correlateMessage.getProcessInstanceId())) {
-            messageCorrelationBuilder = messageCorrelationBuilder.processInstanceId(correlateMessage.getProcessInstanceId());
+            correlationMessageDto.setProcessInstanceId(correlateMessage.getProcessInstanceId());
         }
 
         if (StringUtils.isNotBlank(correlateMessage.getBusinessKey())) {
-            messageCorrelationBuilder = messageCorrelationBuilder.processInstanceBusinessKey(correlateMessage.getBusinessKey());
+            correlationMessageDto.setBusinessKey(correlateMessage.getBusinessKey());
         }
 
-        messageCorrelationBuilder
-                .setVariables(correlateMessage.getPayloadVariables() != null ? correlateMessage.getPayloadVariables() : Map.of())
-                .setVariablesLocal(correlateMessage.getPayloadVariablesLocal() != null ? correlateMessage.getPayloadVariablesLocal() : Map.of())
-                .correlate();
+        this.messageApi.deliverMessage(correlationMessageDto);
     }
 
 }
