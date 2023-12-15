e21c343cb1a2924f97d76fa6bb35954a56bca582
Lukas Mösle <lukas95m@yahoo.de>
2023-12-15 10:55:59 +0100

    bugfix: notify assignees only on assignment event

--- digiwf-engine/digiwf-engine-service/src/main/java/de/muenchen/oss/digiwf/humantask/process/listener/UserTaskNotificationListener.java
+++ digiwf-engine/digiwf-engine-service/src/main/java/de/muenchen/oss/digiwf/humantask/process/listener/UserTaskNotificationListener.java
@@ -80,6 +80,8 @@ public class UserTaskNotificationListener {
             }
         }
         // send notification on task assignment for assignee
+        // assignment event is used for direct user assignment
+        // update event is used for group task assignment
         else if (delegateTask.getEventName().equals("assignment")) {
             log.debug("Notification for created task: {}", delegateTask.getName());
             val notify = NOTIFICATION_SEND.from(delegateTask).getOptional();
