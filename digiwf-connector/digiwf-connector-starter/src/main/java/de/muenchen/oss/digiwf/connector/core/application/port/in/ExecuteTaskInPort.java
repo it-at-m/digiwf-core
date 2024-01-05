43c88724186daa074611e11a111361405beb892f
Lukas Mösle <lukas95m@yahoo.de>
2024-01-05 15:54:09 +0100

    fix: compiling error

--- digiwf-connector/digiwf-connector-starter/src/main/java/de/muenchen/oss/digiwf/connector/core/application/port/in/ExecuteTaskInPort.java
+++ digiwf-connector/digiwf-connector-starter/src/main/java/de/muenchen/oss/digiwf/connector/core/application/port/in/ExecuteTaskInPort.java
@@ -1,6 +1,7 @@
 package de.muenchen.oss.digiwf.connector.core.application.port.in;
 
 import de.muenchen.oss.digiwf.connector.core.domain.IntegrationNameConfigException;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
@@ -8,7 +9,7 @@ import java.util.Map;
 
 public interface ExecuteTaskInPort {
 
-    void executeTask(ExecuteTaskCommand command) throws IntegrationNameConfigException;
+    void executeTask(@Valid ExecuteTaskCommand command) throws IntegrationNameConfigException;
 
     @Data
     @ToString
