32874e684699c5456c2eb376934e8529bc858858
Lukas Mösle <lukas95m@yahoo.de>
2024-01-11 13:18:27 +0100

    chore: fix tests

--- digiwf-connector/digiwf-connector-starter/src/test/java/de/muenchen/oss/digiwf/connector/core/application/usecase/ExecuteTaskUseCaseTest.java
+++ digiwf-connector/digiwf-connector-starter/src/test/java/de/muenchen/oss/digiwf/connector/core/application/usecase/ExecuteTaskUseCaseTest.java
@@ -39,7 +39,7 @@ class ExecuteTaskUseCaseTest {
         useCase.executeTask(command);
 
         // Assert
-        verify(emitEventOutPort).emitEvent("defaultDestination", command.getType(), command.getInstanceId(), command.getIntegrationName(), command.getData());
+        verify(emitEventOutPort).emitEvent("defaultDestination", command.getType(), command.getIntegrationName(), command.getInstanceId(), command.getData());
     }
 
     @Test
@@ -57,7 +57,7 @@ class ExecuteTaskUseCaseTest {
         useCase.executeTask(command);
 
         // Assert
-        verify(emitEventOutPort).emitEvent(command.getCustomDestination(), command.getType(), command.getInstanceId(), command.getIntegrationName(), command.getData());
+        verify(emitEventOutPort).emitEvent(command.getCustomDestination(), command.getType(), command.getIntegrationName(), command.getInstanceId(), command.getData());
     }
 
     @Test
@@ -75,7 +75,7 @@ class ExecuteTaskUseCaseTest {
         useCase.executeTask(command);
 
         // Assert
-        verify(emitEventOutPort).emitEvent(command.getCustomDestination(), command.getType(), command.getInstanceId(), command.getIntegrationName(), command.getData());
+        verify(emitEventOutPort).emitEvent(command.getCustomDestination(), command.getType(), command.getIntegrationName(), command.getInstanceId(), command.getData());
     }
 
     @Test
