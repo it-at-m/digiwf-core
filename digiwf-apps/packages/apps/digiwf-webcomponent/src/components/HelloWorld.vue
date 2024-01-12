d145b826127cf8dddb9f3d39bb76b1256755fd42
devtobi <28538704+devtobi@users.noreply.github.com>
2024-01-12 13:31:52 +0100

    :tada: Created simple HelloWorld WebComponent

---
+++ digiwf-apps/packages/apps/digiwf-webcomponent/src/components/HelloWorld.vue
@@ -0,0 +1,9 @@
+<template>
+  <p>{{ hello }}</p>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const hello = ref("Hello World");
+</script>
---
+++ digiwf-apps/packages/apps/digiwf-webcomponent/src/digiwf-widget.ce.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+    <hello-world />
+  </main>
+</template>
+
+<script setup lang="ts">
+import HelloWorld from "@/components/HelloWorld.vue";
+</script>
+
+<style>
+@import "@/assets/stylesheets/custom.css";
+
+:host {
+  font-family:
+    Open Sans,
+    sans-serif;
+}
+</style>
