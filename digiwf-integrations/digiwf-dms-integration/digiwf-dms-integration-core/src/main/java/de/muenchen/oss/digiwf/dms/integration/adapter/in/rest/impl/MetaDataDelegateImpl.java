ccee7072d516ba0f82ecf1a1e479fe9c716046a4
Julia Lehmann <julia.lehmann@muenchen.de>
2024-01-18 11:39:15 +0100

    #881: add rest endpoint

---
+++ digiwf-integrations/digiwf-dms-integration/digiwf-dms-integration-core/src/main/java/de/muenchen/oss/digiwf/dms/integration/adapter/in/rest/impl/MetaDataDelegateImpl.java
@@ -0,0 +1,13 @@
+package de.muenchen.oss.digiwf.dms.integration.adapter.in.rest.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import de.muenchen.oss.digiwf.dms.integration.application.port.in.rest.api.MetaDataDelegate;
+
+@Component
+@RequiredArgsConstructor
+public class MetaDataDelegateImpl implements MetaDataDelegate {
+
+
+
+}
