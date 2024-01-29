c6aa236c9d4d2addfcf52c8558ed0e1f0a3d1ec6
dominikhorn <dominik.horn93@gmail.com>
2024-01-29 15:04:57 +0100

    feat: add ticket example

--- digiwf-integrations/digiwf-ticket-integration/digiwf-ticket-integration-core/src/main/java/de/muenchen/oss/digiwf/ticket/integration/adapter/in/streaming/WriteArticleDto.java
+++ digiwf-integrations/digiwf-ticket-integration/digiwf-ticket-integration-core/src/main/java/de/muenchen/oss/digiwf/ticket/integration/adapter/in/streaming/WriteArticleDto.java
@@ -1,6 +1,5 @@
 package de.muenchen.oss.digiwf.ticket.integration.adapter.in.streaming;
 
-import de.muenchen.oss.digiwf.ticket.integration.domain.model.TicketStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,6 +12,6 @@ public class WriteArticleDto {
     private String ticketId;
     private String article;
     private String userId;
-    private TicketStatus status;
+    private String status;
 
 }
