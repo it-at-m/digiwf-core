37dadde7191bf7378176cd8b9725c54be7dab29c
Lukas Mösle <lukas95m@yahoo.de>
2024-01-31 11:45:05 +0100

    bugfix(#942): ajdust form builder settings for all field types

--- digiwf-apps/packages/components/digiwf-form-builder-settings/src/en/FormFields.ts
+++ digiwf-apps/packages/components/digiwf-form-builder-settings/src/en/FormFields.ts
@@ -150,6 +150,101 @@ const basicOptions = {
                 "outlined": true,
                 "dense": true
               }
+            }
+          }
+        }
+      }
+    }
+  }
+};
+
+const optionsWithCustomErrorMessages = {
+  "title": "Optionen",
+  "type": "object",
+  "properties": {
+    "x-props": {
+      "type": "object",
+      "description": "Ui",
+      "properties": {
+        "dense": {
+          "type": "boolean",
+          "title": "Dense",
+          "x-props": {
+            "outlined": true,
+            "dense": true
+          },
+          "x-options": {
+            "fieldColProps": {
+              "cols": 12,
+              "sm": 6
+            }
+          }
+        },
+        "outlined": {
+          "type": "boolean",
+          "title": "Outlined",
+          "x-props": {
+            "outlined": true,
+            "dense": true
+          },
+          "x-options": {
+            "fieldColProps": {
+              "cols": 12,
+              "sm": 6
+            }
+          }
+        }
+      }
+    },
+    "x-options": {
+      "type": "object",
+      "properties": {
+        "fieldColProps": {
+          "description": "Größe (max. 12)",
+          "type": "object",
+          "properties":
+            {
+              "sm": {
+                "type": "integer",
+                "title": "Standardgröße",
+                "x-props": {
+                  "outlined": true,
+                  "dense": true
+                },
+                "x-options": {
+                  "fieldColProps": {
+                    "cols": 12,
+                    "sm": 6
+                  }
+                }
+              },
+              "cols": {
+                "type": "integer",
+                "title": "Größe auf kleinen Geräten",
+                "x-props": {
+                  "outlined": true,
+                  "dense": true
+                },
+                "x-options": {
+                  "fieldColProps": {
+                    "cols": 12,
+                    "sm": 6
+                  }
+                }
+              },
+            }
+        },
+        "messages": {
+          "type": "object",
+          "description": "Messages",
+          "properties": {
+            "pattern": {
+              "type": "string",
+              "title": "Pattern (Error Message)",
+              "x-props": {
+                "outlined": true,
+                "dense": true
+              }
             },
             "minLength": {
               "type": "string",
@@ -171,11 +266,121 @@ const basicOptions = {
         }
       }
     }
-
   }
 };
 
-const basicArrayOptions = {
+const optionsNumberWithCustomErrorMessages = {
+  "title": "Optionen",
+  "type": "object",
+  "properties": {
+    "x-props": {
+      "type": "object",
+      "description": "Ui",
+      "properties": {
+        "dense": {
+          "type": "boolean",
+          "title": "Dense",
+          "x-props": {
+            "outlined": true,
+            "dense": true
+          },
+          "x-options": {
+            "fieldColProps": {
+              "cols": 12,
+              "sm": 6
+            }
+          }
+        },
+        "outlined": {
+          "type": "boolean",
+          "title": "Outlined",
+          "x-props": {
+            "outlined": true,
+            "dense": true
+          },
+          "x-options": {
+            "fieldColProps": {
+              "cols": 12,
+              "sm": 6
+            }
+          }
+        }
+      }
+    },
+    "x-options": {
+      "type": "object",
+      "properties": {
+        "fieldColProps": {
+          "description": "Größe (max. 12)",
+          "type": "object",
+          "properties":
+            {
+              "sm": {
+                "type": "integer",
+                "title": "Standardgröße",
+                "x-props": {
+                  "outlined": true,
+                  "dense": true
+                },
+                "x-options": {
+                  "fieldColProps": {
+                    "cols": 12,
+                    "sm": 6
+                  }
+                }
+              },
+              "cols": {
+                "type": "integer",
+                "title": "Größe auf kleinen Geräten",
+                "x-props": {
+                  "outlined": true,
+                  "dense": true
+                },
+                "x-options": {
+                  "fieldColProps": {
+                    "cols": 12,
+                    "sm": 6
+                  }
+                }
+              },
+            }
+        },
+        "messages": {
+          "type": "object",
+          "description": "Messages",
+          "properties": {
+            "pattern": {
+              "type": "string",
+              "title": "Pattern (Error Message)",
+              "x-props": {
+                "outlined": true,
+                "dense": true
+              }
+            },
+            "minimum": {
+              "type": "string",
+              "title": "The value must be greater than or equal to {minimum}",
+              "x-props": {
+                "outlined": true,
+                "dense": true
+              }
+            },
+            "maximum": {
+              "type": "string",
+              "title": "The value must be lower than or equal to {maximum}",
+              "x-props": {
+                "outlined": true,
+                "dense": true
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+};
+
+const optionsArrayWithCustomErrorMessages = {
   "title": "Optionen",
   "type": "object",
   "properties": {
@@ -330,7 +535,7 @@ const textFeldSchema = {
       }
     },
     {
-      ...basicOptions
+      ...optionsWithCustomErrorMessages
     },
     {
       ...basicValidation,
@@ -375,7 +580,7 @@ const integerSchema = {
       }
     },
     {
-      ...basicOptions
+      ...optionsNumberWithCustomErrorMessages
     },
     {
       ...basicValidation,
@@ -420,7 +625,7 @@ const numberSchema = {
       }
     },
     {
-      ...basicOptions
+      ...optionsNumberWithCustomErrorMessages
     },
     {
       ...basicValidation,
@@ -499,7 +704,7 @@ const textAreaSchema = {
       }
     },
     {
-      ...basicOptions
+      ...optionsWithCustomErrorMessages
     },
     {
       ...basicValidation,
@@ -890,7 +1095,7 @@ const selectSchema = {
       }
     },
     {
-      ...basicArrayOptions
+      ...basicOptions
     },
     {
       ...basicValidation
@@ -984,7 +1189,7 @@ const multiselectSchema = {
       }
     },
     {
-      ...basicArrayOptions
+      ...optionsArrayWithCustomErrorMessages
     },
     {
       ...basicValidation,
@@ -1208,7 +1413,7 @@ const multiUserinputSchema = {
       }
     },
     {
-      ...basicArrayOptions
+      ...optionsArrayWithCustomErrorMessages
     },
     {
       ...basicValidation,
@@ -1281,7 +1486,7 @@ const arrayInput = {
       }
     },
     {
-      ...basicArrayOptions
+      ...optionsArrayWithCustomErrorMessages
     },
     {
       ...basicValidation,
@@ -1332,7 +1537,7 @@ const arrayObjectInput = {
       }
     },
     {
-      ...basicArrayOptions
+      ...optionsArrayWithCustomErrorMessages
     },
     {
       ...basicValidation,
