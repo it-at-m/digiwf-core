541ab0b61befc2175832fc03ea04cc84c494a06a
Lukas Mösle <lukas95m@yahoo.de>
2024-02-01 18:38:43 +0100

    bugfix(#1253): configure dlq to raise incidents and fix IncidentAdapter in connector

--- digiwf-integrations/digiwf-email-integration/digiwf-email-integration-core/src/main/java/de/muenchen/oss/digiwf/email/integration/adapter/in/MessageProcessor.java
+++ digiwf-integrations/digiwf-email-integration/digiwf-email-integration-core/src/main/java/de/muenchen/oss/digiwf/email/integration/adapter/in/MessageProcessor.java
@@ -4,6 +4,9 @@ import de.muenchen.oss.digiwf.email.integration.application.port.in.SendMail;
 import de.muenchen.oss.digiwf.email.integration.infrastructure.MonitoringService;
 import de.muenchen.oss.digiwf.email.integration.model.Mail;
 import de.muenchen.oss.digiwf.message.process.api.ErrorApi;
+import de.muenchen.oss.digiwf.message.process.api.error.BpmnError;
+import de.muenchen.oss.digiwf.message.process.api.error.IncidentError;
+import jakarta.validation.ValidationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.Message;
 
@@ -20,23 +23,23 @@ public class MessageProcessor {
 
     public Consumer<Message<Mail>> emailIntegration() {
         return message -> {
-//            try {
+            try {
                 this.mailUseCase.sendMail(
                         message.getHeaders().get(DIGIWF_PROCESS_INSTANCE_ID, String.class),
                         message.getHeaders().get(TYPE, String.class),
                         message.getHeaders().get(DIGIWF_INTEGRATION_NAME, String.class),
                         message.getPayload());
                 this.monitoringService.sendMailSucceeded();
-//            } catch (final BpmnError bpmnError) {
-//                this.monitoringService.sendMailFailed();
-//                this.errorApi.handleBpmnError(message.getHeaders(), bpmnError);
-//            } catch (final ValidationException validationException) {
-//                this.monitoringService.sendMailFailed();
-//                this.errorApi.handleBpmnError(message.getHeaders(), new BpmnError("VALIDATION_ERROR", validationException.getMessage()));
-//            } catch (final IncidentError incidentError) {
-//                this.monitoringService.sendMailFailed();
-//                this.errorApi.handleIncident(message.getHeaders(), incidentError);
-//            }
+            } catch (final BpmnError bpmnError) {
+                this.monitoringService.sendMailFailed();
+                this.errorApi.handleBpmnError(message.getHeaders(), bpmnError);
+            } catch (final ValidationException validationException) {
+                this.monitoringService.sendMailFailed();
+                this.errorApi.handleBpmnError(message.getHeaders(), new BpmnError("VALIDATION_ERROR", validationException.getMessage()));
+            } catch (final IncidentError incidentError) {
+                this.monitoringService.sendMailFailed();
+                this.errorApi.handleIncident(message.getHeaders(), incidentError);
+            }
         };
     }
 
