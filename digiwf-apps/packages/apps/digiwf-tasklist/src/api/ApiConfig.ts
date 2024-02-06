612c2905c682d73ef4773077f15f2f9532e4ef14
Julia Lehmann <julia.lehmann@muenchen.de>
2024-02-06 16:39:22 +0100

    #881: add variable query

--- digiwf-apps/packages/apps/digiwf-tasklist/src/api/ApiConfig.ts
+++ digiwf-apps/packages/apps/digiwf-tasklist/src/api/ApiConfig.ts
@@ -4,7 +4,7 @@ export class ApiConfig {
 
   public static base: string = import.meta.env.VITE_VUE_APP_API_URL ? import.meta.env.VITE_VUE_APP_API_URL : 'api/digitalwf-backend-service';
   public static tasklistBase: string = import.meta.env.VITE_VUE_APP_TASKLIST_API_URL ? import.meta.env.VITE_VUE_APP_TASKLIST_API_URL : 'api/digitalwf-tasklist-service/rest';
-  public static mucsDmsBase: string = 'api/dms-integration/mucs/rest/';
+  public static mucsDmsBase: string = import.meta.env.VITE_VUE_APP_DMS_API_URL ? import.meta.env.VITE_VUE_APP_DMS_API_URL : 'api/dms-integration/mucs/rest/';
 
   /**
    * @deprecated
