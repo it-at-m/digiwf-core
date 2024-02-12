961f37f326861d64f3b10af2ffab30d820330f47
stephan.strehler <stephan.strehler@cgi.com>
2024-02-12 16:31:59 +0100

    #1251: move event EventMessageCountingMonitor setup to constructor. add /actuator/prometheus to permitted paths

--- digiwf-task/digiwf-tasklist-service/src/main/java/de/muenchen/oss/digiwf/task/service/infra/metrics/EventMessageCountingMonitor.java
+++ digiwf-task/digiwf-tasklist-service/src/main/java/de/muenchen/oss/digiwf/task/service/infra/metrics/EventMessageCountingMonitor.java
@@ -8,47 +8,42 @@ import org.axonframework.messaging.Message;
 import org.axonframework.monitoring.MessageMonitor;
 import org.jetbrains.annotations.NotNull;
 
-@RequiredArgsConstructor
 public class EventMessageCountingMonitor implements MessageMonitor<Message<?>> {
 
-    private final MeterRegistry meterRegistry;
-    private final String meterName;
+  private final Counter ingestedCounter;
+  private final Counter successCounter;
+  private final Counter failureCounter;
+  private final Counter processedCounter;
+  private final Counter ignoredCounter;
 
-    private Counter ingestedCounter;
-    private Counter successCounter;
-    private Counter failureCounter;
-    private Counter processedCounter;
-    private Counter ignoredCounter;
+  public EventMessageCountingMonitor(MeterRegistry meterRegistry, String meterName) {
+    ingestedCounter = meterRegistry.counter(meterName, "status", "ingested");
+    successCounter = meterRegistry.counter(meterName, "status", "success");
+    failureCounter = meterRegistry.counter(meterName, "status", "failure");
+    processedCounter = meterRegistry.counter(meterName, "status", "processed");
+    ignoredCounter = meterRegistry.counter(meterName, "status", "ignored");
+  }
 
-    @PostConstruct
-    public void setupMonitor() {
-        ingestedCounter = meterRegistry.counter(meterName, "status", "ingested");
-        successCounter = meterRegistry.counter(meterName, "status", "success");
-        failureCounter = meterRegistry.counter(meterName, "status", "failure");
-        processedCounter = meterRegistry.counter(meterName, "status", "processed");
-        ignoredCounter = meterRegistry.counter(meterName, "status", "ignored");
-    }
+  @Override
+  public MonitorCallback onMessageIngested(@NotNull Message<?> message) {
+    ingestedCounter.increment();
+    return new MessageMonitor.MonitorCallback() {
+      @Override
+      public void reportSuccess() {
+        processedCounter.increment();
+        successCounter.increment();
+      }
 
-    @Override
-    public MonitorCallback onMessageIngested(@NotNull Message<?> message) {
-        ingestedCounter.increment();
-        return new MessageMonitor.MonitorCallback() {
-            @Override
-            public void reportSuccess() {
-                processedCounter.increment();
-                successCounter.increment();
-            }
+      @Override
+      public void reportFailure(Throwable cause) {
+        processedCounter.increment();
+        failureCounter.increment();
+      }
 
-            @Override
-            public void reportFailure(Throwable cause) {
-                processedCounter.increment();
-                failureCounter.increment();
-            }
-
-            @Override
-            public void reportIgnored() {
-                ignoredCounter.increment();
-            }
-        };
-    }
+      @Override
+      public void reportIgnored() {
+        ignoredCounter.increment();
+      }
+    };
+  }
 }
