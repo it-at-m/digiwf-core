032ad671b7f9e3d9d0e252b65ad4d4695e791823
darenegade <darenegade@users.noreply.github.com>
2024-02-21 16:34:18 +0100

    Fix: DMS-Input returns COOs

--- digiwf-apps/packages/components/digiwf-dms-input/src/components/DwfDmsInput.vue
+++ digiwf-apps/packages/components/digiwf-dms-input/src/components/DwfDmsInput.vue
@@ -134,8 +134,8 @@ export default defineComponent({
 
     const getMetadataOfObjects = (objects: DmsObject[]) => {
       return objects
-          .map(doc => doc.metadata)
-          .filter(metadata => !!metadata);
+          .filter(doc => !!doc.metadata)
+          .map(doc => doc.coo);
     }
 
     const validate = (number: number) => {
