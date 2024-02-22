77dd0ba4bdb300c1338f51d6e6c730c029b18d0e
devtobi <28538704+devtobi@users.noreply.github.com>
2024-02-22 15:53:30 +0100

    :construction: Added pagination relevant code to ServiceInstanceCard

--- digiwf-apps/packages/apps/digiwf-webcomponent/src/components/ServiceInstanceCard.vue
+++ digiwf-apps/packages/apps/digiwf-webcomponent/src/components/ServiceInstanceCard.vue
@@ -3,20 +3,25 @@
     :link-path="FRONTEND_INSTANCE_PATH"
     card-title="Neueste Anträge"
     link-text="Alle Anträge ansehen (DigiWF)"
-    :loading="loading"
+    :loading="showLoading"
+    :page-data="pageData"
+    @reload="reload"
+    @changepage="changePage"
   >
     <template #content>
       <c-list-group flush>
-        <service-instance-list-item
-          v-for="serviceInstance in SERVICE_INSTANCES_DUMMIES"
-          :key="serviceInstance.id"
-          :service-instance="serviceInstance"
-        />
+        <template v-if="serviceInstanceData &&  serviceInstanceData.content">
+          <service-instance-list-item
+            v-for="serviceInstance in serviceInstanceData.content"
+            :key="serviceInstance.id"
+            :service-instance="serviceInstance"
+          />
+        </template>
       </c-list-group>
     </template>
     <template #placeholder>
       <c-list-group flush>
-        <service-instance-list-item-placeholder v-for="i in 3" :key="i"/>
+        <service-instance-list-item-placeholder v-for="i in pageSize" :key="i"/>
       </c-list-group>
     </template>
   </widget-card>
@@ -24,22 +29,71 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue";
+import { computed, ref, watch } from "vue";
 import { CListGroup } from "@coreui/vue";
 
 import WidgetCard from "@/components/common/WidgetCard.vue";
 import ServiceInstanceListItem from "@/components/ServiceInstanceListItem.vue";
 import { useHasAccessToken } from "@/composables/useAccessToken";
 import { useServiceInstanceControllerAPI } from "@/composables/useServiceInstanceControllerAPI";
-import SERVICE_INSTANCES_DUMMIES from "@/dev/dummy-data";
+import PAGE_SERVICE_INSTANCE_DUMMY from "@/dev/dummy-data";
 import { FRONTEND_INSTANCE_PATH } from "@/util/constants";
 import ServiceInstanceListItemPlaceholder from "@/components/placeholders/ServiceInstanceListItemPlaceholder.vue";
-
-const { hasAccessToken } = useHasAccessToken();
-
-const loading = computed(() => !hasAccessToken?.value);
+import type { PageData } from "@/types/PageData";
+import type { PageServiceInstanceTO } from "@muenchen/digiwf-engine-api-internal";
 
 const { callGetAssignedProcessInstances } = useServiceInstanceControllerAPI();
+
+const { hasAccessToken } = useHasAccessToken();
+const loading = ref(false);
+const showLoading = computed(() => !hasAccessToken?.value || loading.value);
+
+const pageSize = 3;
+const page = ref(0);
+
+const serviceInstanceData = ref<PageServiceInstanceTO>(null);
+
+watch(hasAccessToken, (token: boolean) => {
+  if(token) {
+    // TODO REPLACE WITH REAL CALL;
+    getProcessInstances();
+  }
+})
+
+const pageData = computed<PageData>(() => {
+  if (!serviceInstanceData.value) return undefined;
+  const { totalPages, totalElements } = serviceInstanceData.value;
+  return {
+    totalPages,
+    number: page.value,
+    totalElements
+  };
+});
+
+const reload = () => {
+  // TODO IMPLEMENT
+  console.log("TODO IMPLEMENT");
+}
+
+const changePage = (newPage: number) => {
+  page.value = newPage;
+}
+
+watch(page, () => {
+  if(hasAccessToken?.value) {
+    // TODO REPLACE WITH REAL CALL
+    getProcessInstances();
+  }
+});
+
+// TODO REPLACE METHOD WITH REAL CALL
+const getProcessInstances = async () => {
+  loading.value = true
+  setTimeout(() => {
+    serviceInstanceData.value = PAGE_SERVICE_INSTANCE_DUMMY;
+    loading.value = false;
+  }, 2000);
+}
 
 const sendTest = async () => {
   const result = await callGetAssignedProcessInstances(1, 2, "test");
