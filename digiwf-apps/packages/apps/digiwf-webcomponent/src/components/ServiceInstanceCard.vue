bf0fc031da0422d2f699a6a6212b2b207e0c89f4
devtobi <28538704+devtobi@users.noreply.github.com>
2024-02-22 14:24:53 +0100

    :recycle: Removed unnecessary component to simplify templating

--- digiwf-apps/packages/apps/digiwf-webcomponent/src/components/ServiceInstanceCard.vue
+++ digiwf-apps/packages/apps/digiwf-webcomponent/src/components/ServiceInstanceCard.vue
@@ -6,14 +6,18 @@
     :loading="loading"
   >
     <template #content>
-      <service-instance-list-item
-        v-for="serviceInstance in SERVICE_INSTANCES_DUMMIES"
-        :key="serviceInstance.id"
-        :service-instance="serviceInstance"
-      />
+      <c-list-group flush>
+        <service-instance-list-item
+          v-for="serviceInstance in SERVICE_INSTANCES_DUMMIES"
+          :key="serviceInstance.id"
+          :service-instance="serviceInstance"
+        />
+      </c-list-group>
     </template>
     <template #placeholder>
-      <service-instance-list-placeholder />
+      <c-list-group flush>
+        <service-instance-list-item-placeholder v-for="i in 3" :key="i"/>
+      </c-list-group>
     </template>
   </widget-card>
   <button @click="sendTest">TEST API CODE</button>
@@ -21,14 +25,15 @@
 
 <script setup lang="ts">
 import { computed } from "vue";
+import { CListGroup } from "@coreui/vue";
 
 import WidgetCard from "@/components/common/WidgetCard.vue";
-import ServiceInstanceListPlaceholder from "@/components/placeholders/ServiceInstanceListPlaceholder.vue";
 import ServiceInstanceListItem from "@/components/ServiceInstanceListItem.vue";
 import { useHasAccessToken } from "@/composables/useAccessToken";
 import { useServiceInstanceControllerAPI } from "@/composables/useServiceInstanceControllerAPI";
 import SERVICE_INSTANCES_DUMMIES from "@/dev/dummy-data";
 import { FRONTEND_INSTANCE_PATH } from "@/util/constants";
+import ServiceInstanceListItemPlaceholder from "@/components/placeholders/ServiceInstanceListItemPlaceholder.vue";
 
 const { hasAccessToken } = useHasAccessToken();
 
