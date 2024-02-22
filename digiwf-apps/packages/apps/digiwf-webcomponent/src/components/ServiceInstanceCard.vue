77bf9ff86f4d4f579d3ffc59644b692b08f6acab
devtobi <28538704+devtobi@users.noreply.github.com>
2024-02-22 14:18:31 +0100

    :monocle_face: Moved dummy data

--- digiwf-apps/packages/apps/digiwf-webcomponent/src/components/ServiceInstanceCard.vue
+++ digiwf-apps/packages/apps/digiwf-webcomponent/src/components/ServiceInstanceCard.vue
@@ -6,7 +6,11 @@
     :loading="loading"
   >
     <template #content>
-      <service-instance-list />
+      <service-instance-list-item
+        v-for="serviceInstance in SERVICE_INSTANCES_DUMMIES"
+        :key="serviceInstance.id"
+        :service-instance="serviceInstance"
+      />
     </template>
     <template #placeholder>
       <service-instance-list-placeholder />
@@ -20,9 +24,10 @@ import { computed } from "vue";
 
 import WidgetCard from "@/components/common/WidgetCard.vue";
 import ServiceInstanceListPlaceholder from "@/components/placeholders/ServiceInstanceListPlaceholder.vue";
-import ServiceInstanceList from "@/components/ServiceInstanceList.vue";
+import ServiceInstanceListItem from "@/components/ServiceInstanceListItem.vue";
 import { useHasAccessToken } from "@/composables/useAccessToken";
 import { useServiceInstanceControllerAPI } from "@/composables/useServiceInstanceControllerAPI";
+import SERVICE_INSTANCES_DUMMIES from "@/dev/dummy-data";
 import { FRONTEND_INSTANCE_PATH } from "@/util/constants";
 
 const { hasAccessToken } = useHasAccessToken();
