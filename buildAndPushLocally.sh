e85b3d38c065e12b2a061180474882418bcb351e
Lukas Mösle <lukas95m@yahoo.de>
2024-02-22 16:08:39 +0100

    fix: add local build script to navigate around release issues

--- buildAndPushLocally.sh
+++ buildAndPushLocally.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # build
-mvn clean verify -DskipTests -T 16
+./mvnw clean verify -DskipTests -T 16
 
 # build and push digiwf-engine-community image
 docker build -t lmoesle/digiwf-engine-community:latest digiwf-engine/digiwf-engine-service
