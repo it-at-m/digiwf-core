45af68b984109503a3e0f777e32d7a565ef24902
Simon Zambrovski <simon.zambrovski@holisticon.de>
2024-02-23 10:53:24 +0100

    configurble metrics, fix #1338 (#1353)
    * configurble metrics, fix #1338
    
    * Update digiwf-engine/digiwf-engine-service/src/main/resources/application.yml
    
    Co-authored-by: Simon Hirtreiter <simon.hirtreiter@muenchen.de>
    
    * Update digiwf-libs/digiwf-camunda-prometheus/digiwf-camunda-prometheus-starter/src/main/java/de/muenchen/oss/digiwf/camunda/prometheus/MetricsProviderSchedulerAutoConfiguration.java
    
    Co-authored-by: Simon Hirtreiter <simon.hirtreiter@muenchen.de>
    
    * Update digiwf-libs/digiwf-camunda-prometheus/digiwf-camunda-prometheus-starter/src/main/java/de/muenchen/oss/digiwf/camunda/prometheus/CamundaPrometheusProperties.java
    
    Co-authored-by: Simon Hirtreiter <simon.hirtreiter@muenchen.de>
    
    ---------
    
    Co-authored-by: Simon Hirtreiter <simon.hirtreiter@muenchen.de>

--- digiwf-libs/digiwf-camunda-prometheus/digiwf-camunda-prometheus-starter/src/main/java/de/muenchen/oss/digiwf/camunda/prometheus/CamundaPrometheusAutoConfiguration.java
+++ digiwf-libs/digiwf-camunda-prometheus/digiwf-camunda-prometheus-starter/src/main/java/de/muenchen/oss/digiwf/camunda/prometheus/CamundaPrometheusAutoConfiguration.java
@@ -1,38 +1,11 @@
 package de.muenchen.oss.digiwf.camunda.prometheus;
 
-import io.prometheus.client.CollectorRegistry;
-import jakarta.annotation.PostConstruct;
-import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 
-import java.util.List;
 
-@EnableScheduling
-@RequiredArgsConstructor
 @Configuration
+@EnableConfigurationProperties(CamundaPrometheusProperties.class)
 public class CamundaPrometheusAutoConfiguration {
-
-    private final List<MetricsProvider> metricsProviders;
-    private final List<MetricsReporter> metricsReporters;
-    private final CollectorRegistry collectorRegistry;
-
-    @Scheduled(fixedDelayString = "${io.muenchendigital.camunda.prometheus.update-interval}")
-    public void updateMetrics() {
-        metricsProviders.forEach(MetricsProvider::updateMetrics);
-    }
-
-    @PostConstruct
-    public void initializeMetrics() {
-        // providers
-        metricsProviders.forEach(provider -> provider.registerMetrics(collectorRegistry));
-
-        // reporters
-        metricsReporters.forEach(provider -> provider.registerMetrics(collectorRegistry));
-    }
-
-
 
 }
