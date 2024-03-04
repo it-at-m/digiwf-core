b3240187bdb38e51b1f027ae919d2fa95650aa29
René Zarwel <rene.zarwel@muenchen.de>
2024-03-04 13:08:20 +0100

    remove from eventlistener

--- digiwf-engine/digiwf-engine-service/src/main/java/de/muenchen/oss/digiwf/process/definition/api/streaming/ProcessEventListener.java
+++ digiwf-engine/digiwf-engine-service/src/main/java/de/muenchen/oss/digiwf/process/definition/api/streaming/ProcessEventListener.java
@@ -8,7 +8,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.function.Consumer;
@@ -29,7 +28,6 @@ public class ProcessEventListener {
     private final UserService userService;
 
     @Bean
-    @Transactional
     public Consumer<Message<StartInstanceTOV01>> startProcessV01() {
         return startInstance -> {
             log.info("Received process start {}", startInstance.getPayload());
