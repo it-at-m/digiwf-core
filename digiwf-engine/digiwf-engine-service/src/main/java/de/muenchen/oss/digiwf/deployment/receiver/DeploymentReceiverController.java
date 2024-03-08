da5c0f6ec9dff22e2dc4488c1fd5a591b3166883
Lukas Mösle <lukas95m@yahoo.de>
2024-03-08 13:40:30 +0100

    feature: enable deployment proxy only with profile local

--- digiwf-engine/digiwf-engine-service/src/main/java/de/muenchen/oss/digiwf/deployment/receiver/DeploymentReceiverController.java
+++ digiwf-engine/digiwf-engine-service/src/main/java/de/muenchen/oss/digiwf/deployment/receiver/DeploymentReceiverController.java
@@ -6,12 +6,15 @@ import io.miragon.miranum.deploymentreceiver.domain.DeploymentStatus;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
+// Note: Deployment Server ist not deployed and this controller is only needed for local development at the moment
+@Profile("local")
 @RestController
 @RequestMapping("/rest/deployment/v2")
 @RequiredArgsConstructor
