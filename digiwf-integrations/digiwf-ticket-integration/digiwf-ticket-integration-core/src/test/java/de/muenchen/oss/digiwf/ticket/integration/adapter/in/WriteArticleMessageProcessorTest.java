b1fcd9a0e92d8d1b7523a8505e0cb9fd4354b6fb
Lukas Mösle <lukas95m@yahoo.de>
2024-03-11 15:38:29 +0100

    chore: fix unit testss

--- digiwf-integrations/digiwf-ticket-integration/digiwf-ticket-integration-core/src/test/java/de/muenchen/oss/digiwf/ticket/integration/adapter/in/WriteArticleMessageProcessorTest.java
+++ digiwf-integrations/digiwf-ticket-integration/digiwf-ticket-integration-core/src/test/java/de/muenchen/oss/digiwf/ticket/integration/adapter/in/WriteArticleMessageProcessorTest.java
@@ -14,6 +14,7 @@ import org.mockito.Mockito;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHeaders;
 
+import java.util.List;
 import java.util.Map;
 
 import static de.muenchen.oss.digiwf.message.common.MessageConstants.*;
@@ -47,7 +48,7 @@ class WriteArticleMessageProcessorTest {
 
             @Override
             public MessageHeaders getHeaders() {
-                return new MessageHeaders(Map.of(DIGIWF_PROCESS_INSTANCE_ID, "processInstanceId", DIGIWF_INTEGRATION_NAME, "dmsIntegration", TYPE, "type"));
+                return new MessageHeaders(Map.of(DIGIWF_PROCESS_INSTANCE_ID, "processInstanceId", DIGIWF_INTEGRATION_NAME, "dmsIntegration", TYPE, "type", DIGIWF_PROCESS_DEFINITION, "processDefinition"));
             }
         };
     }
@@ -71,7 +72,7 @@ class WriteArticleMessageProcessorTest {
         Mockito.verify(writeArticleInPort, Mockito.times(1)).writeArticle(
                 "ticketID123",
                 new Article("mein text", "userID123"),
-                TicketStatus.OPEN, null, null);
+                TicketStatus.OPEN, List.of("test"), "processDefinition");
     }
 
 }
--- digiwf-integrations/digiwf-ticket-integration/digiwf-ticket-integration-core/src/test/java/de/muenchen/oss/digiwf/ticket/integration/application/WriteArticleUseCaseTest.java
+++ digiwf-integrations/digiwf-ticket-integration/digiwf-ticket-integration-core/src/test/java/de/muenchen/oss/digiwf/ticket/integration/application/WriteArticleUseCaseTest.java
@@ -1,19 +1,24 @@
 package de.muenchen.oss.digiwf.ticket.integration.application;
 
+import de.muenchen.oss.digiwf.ticket.integration.application.port.out.LoadFilePort;
 import de.muenchen.oss.digiwf.ticket.integration.application.port.out.TicketOutPort;
 import de.muenchen.oss.digiwf.ticket.integration.domain.model.Article;
 import de.muenchen.oss.digiwf.ticket.integration.domain.model.TicketStatus;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.util.List;
+
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-public class WriteArticleUseCaseTest {
+class WriteArticleUseCaseTest {
 
     private final TicketOutPort ticketOutPort = Mockito.mock(TicketOutPort.class);
+    private final LoadFilePort loadFilePort = Mockito.mock(LoadFilePort.class);
 
-    private final WriteArticleUseCase writeArticleUseCase = new WriteArticleUseCase(ticketOutPort, null);
+    private final WriteArticleUseCase writeArticleUseCase = new WriteArticleUseCase(ticketOutPort, loadFilePort);
 
     @Test
     void writeArticle_callsUpdateTicketWithCorrectArguments() {
@@ -23,10 +28,12 @@ public class WriteArticleUseCaseTest {
         TicketStatus status = TicketStatus.OPEN;
 
         // when
-        writeArticleUseCase.writeArticle(ticketId, article, status, null, null);
+        when(loadFilePort.loadFiles(List.of(), "processDefinition")).thenReturn(List.of());
+
+        writeArticleUseCase.writeArticle(ticketId, article, status, null, "processDefinition");
 
         // then
-        verify(ticketOutPort).updateTicket(eq(ticketId), eq(article), eq(status), null);
+        verify(ticketOutPort).updateTicket(eq(ticketId), eq(article), eq(status), eq(List.of()));
     }
 
 }
