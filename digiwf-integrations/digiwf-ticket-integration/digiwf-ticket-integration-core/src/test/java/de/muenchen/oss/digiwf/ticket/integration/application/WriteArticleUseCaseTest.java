59caa595331311151a8fc3f62ce97c5b680d4951
Lukas Mösle <lukas95m@yahoo.de>
2024-03-11 16:25:34 +0100

    test: add more unit tests

--- digiwf-integrations/digiwf-ticket-integration/digiwf-ticket-integration-core/src/test/java/de/muenchen/oss/digiwf/ticket/integration/application/WriteArticleUseCaseTest.java
+++ digiwf-integrations/digiwf-ticket-integration/digiwf-ticket-integration-core/src/test/java/de/muenchen/oss/digiwf/ticket/integration/application/WriteArticleUseCaseTest.java
@@ -3,6 +3,7 @@ package de.muenchen.oss.digiwf.ticket.integration.application;
 import de.muenchen.oss.digiwf.ticket.integration.application.port.out.LoadFilePort;
 import de.muenchen.oss.digiwf.ticket.integration.application.port.out.TicketOutPort;
 import de.muenchen.oss.digiwf.ticket.integration.domain.model.Article;
+import de.muenchen.oss.digiwf.ticket.integration.domain.model.FileContent;
 import de.muenchen.oss.digiwf.ticket.integration.domain.model.TicketStatus;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -23,17 +24,38 @@ class WriteArticleUseCaseTest {
     @Test
     void writeArticle_callsUpdateTicketWithCorrectArguments() {
         // given
-        String ticketId = "123";
-        Article article = new Article("article", "user");
-        TicketStatus status = TicketStatus.OPEN;
+        final String ticketId = "123";
+        final Article article = new Article("article", "user");
+        final TicketStatus status = TicketStatus.OPEN;
 
         // when
         when(loadFilePort.loadFiles(List.of(), "processDefinition")).thenReturn(List.of());
 
-        writeArticleUseCase.writeArticle(ticketId, article, status, null, "processDefinition");
+        writeArticleUseCase.writeArticle(ticketId, article, status, List.of(), "processDefinition");
 
         // then
         verify(ticketOutPort).updateTicket(eq(ticketId), eq(article), eq(status), eq(List.of()));
+    }
+
+    @Test
+    void writeArticle_callsUpdateTicketWithCorrectArgumentsAndFilePaths() {
+        // given
+        final String ticketId = "123";
+        final Article article = new Article("article", "user");
+        final TicketStatus status = TicketStatus.OPEN;
+        final List<String> filepaths = List.of("test/file/path");
+
+        final FileContent fileContent = new FileContent("text/plain", "file", "content");
+
+
+        // when
+        when(loadFilePort.loadFiles(filepaths, "processDefinition")).thenReturn(
+                List.of(fileContent));
+
+        writeArticleUseCase.writeArticle(ticketId, article, status, filepaths, "processDefinition");
+
+        // then
+        verify(ticketOutPort).updateTicket(eq(ticketId), eq(article), eq(status), eq(List.of(fileContent)));
     }
 
 }
