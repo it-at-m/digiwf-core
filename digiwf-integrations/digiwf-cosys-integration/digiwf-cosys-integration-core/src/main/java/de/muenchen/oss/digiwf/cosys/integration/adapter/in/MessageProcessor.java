f83e30fd68d3233375d9fa1d213ef75bf2621502
Simon Hirtreiter <simon.hirtreiter@muenchen.de>
2024-03-15 12:45:01 +0100

    cosys integration cleanup class names

--- digiwf-integrations/digiwf-cosys-integration/digiwf-cosys-integration-core/src/main/java/de/muenchen/oss/digiwf/cosys/integration/adapter/in/MessageProcessor.java
+++ digiwf-integrations/digiwf-cosys-integration/digiwf-cosys-integration-core/src/main/java/de/muenchen/oss/digiwf/cosys/integration/adapter/in/MessageProcessor.java
@@ -1,7 +1,7 @@
 package de.muenchen.oss.digiwf.cosys.integration.adapter.in;
 
 
-import de.muenchen.oss.digiwf.cosys.integration.application.port.in.CreateDocument;
+import de.muenchen.oss.digiwf.cosys.integration.application.port.in.CreateDocumentInPort;
 import de.muenchen.oss.digiwf.cosys.integration.model.GenerateDocument;
 import de.muenchen.oss.digiwf.message.process.api.ErrorApi;
 import de.muenchen.oss.digiwf.message.process.api.error.BpmnError;
@@ -23,7 +23,7 @@ import static de.muenchen.oss.digiwf.message.common.MessageConstants.*;
 @RequiredArgsConstructor
 public class MessageProcessor {
 
-    private final CreateDocument documentUseCase;
+    private final CreateDocumentInPort createDocumentInPort;
 
     private final ErrorApi errorApi;
 
@@ -37,10 +37,10 @@ public class MessageProcessor {
     public Consumer<Message<GenerateDocument>> cosysIntegration() {
         return message -> {
             try {
-            log.info("Processing generate document request from eventbus");
-            final GenerateDocument document = message.getPayload();
-            log.debug("Generate document request: {}", document);
-                this.documentUseCase.createDocument(
+                log.info("Processing generate document request from eventbus");
+                final GenerateDocument document = message.getPayload();
+                log.debug("Generate document request: {}", document);
+                this.createDocumentInPort.createDocument(
                         message.getHeaders().get(DIGIWF_PROCESS_INSTANCE_ID, String.class),
                         message.getHeaders().get(TYPE, String.class),
                         message.getHeaders().get(DIGIWF_INTEGRATION_NAME, String.class),
--- digiwf-integrations/digiwf-cosys-integration/digiwf-cosys-integration-core/src/main/java/de/muenchen/oss/digiwf/cosys/integration/application/port/out/GenerateDocumentOutPort.java
+++ digiwf-integrations/digiwf-cosys-integration/digiwf-cosys-integration-core/src/main/java/de/muenchen/oss/digiwf/cosys/integration/application/port/out/GenerateDocumentOutPort.java
@@ -3,7 +3,7 @@ package de.muenchen.oss.digiwf.cosys.integration.application.port.out;
 import de.muenchen.oss.digiwf.cosys.integration.model.GenerateDocument;
 import reactor.core.publisher.Mono;
 
-public interface GenerateDocumentPort {
+public interface GenerateDocumentOutPort {
 
     Mono<byte[]> generateCosysDocument(final GenerateDocument generateDocument);
 
