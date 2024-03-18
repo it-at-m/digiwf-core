63c9fb2dea252de4781b97b2b50331c6f0342861
Julia Lehmann <julia.lehmann@muenchen.de>
2024-03-18 14:01:45 +0100

    #1227: use @NotEmpty for Map

--- digiwf-integrations/digiwf-email-integration/digiwf-email-integration-core/src/main/java/de/muenchen/oss/digiwf/email/integration/model/TemplateMail.java
+++ digiwf-integrations/digiwf-email-integration/digiwf-email-integration-core/src/main/java/de/muenchen/oss/digiwf/email/integration/model/TemplateMail.java
@@ -1,6 +1,7 @@
 package de.muenchen.oss.digiwf.email.integration.model;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
@@ -23,7 +24,7 @@ public class TemplateMail extends BasicMail{
     /**
      * Bottom body of the mail.
      */
-    @NotBlank(message = "No content given")
+    @NotEmpty(message = "No content given")
     private Map<String,Object> content;
 
     public TemplateMail(String receivers, String receiversCc, String receiversBcc, String subject, String replyTo, List<PresignedUrl> attachments, String template, Map<String,Object> content) {
