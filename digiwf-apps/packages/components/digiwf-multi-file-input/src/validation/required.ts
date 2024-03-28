91d7cd0d8e8b1008e5b9ea7ae8a34937436bb5cd
stephan.strehler <stephan.strehler@cgi.com>
2024-03-28 10:06:16 +0100

    #1391: fix missing file required marker

--- digiwf-apps/packages/components/digiwf-multi-file-input/src/validation/required.ts
+++ digiwf-apps/packages/components/digiwf-multi-file-input/src/validation/required.ts
@@ -1,6 +1,7 @@
 export const checkRequired = (schema: any) => {
   return (
     schema["x-rules"]?.includes("required") ||
+    schema["x-rules"]?.includes("requiredObject") ||
     schema.minLength > 0 ||
     schema.minItems > 0
   );
