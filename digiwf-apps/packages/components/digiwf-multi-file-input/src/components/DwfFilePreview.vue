4210f86fbe626f84412727aff50299bc9d150a7a
stephan.strehler <stephan.strehler@cgi.com>
2024-03-28 16:48:01 +0100

    #920: add reader support for file input

--- digiwf-apps/packages/components/digiwf-multi-file-input/src/components/DwfFilePreview.vue
+++ digiwf-apps/packages/components/digiwf-multi-file-input/src/components/DwfFilePreview.vue
@@ -1,10 +1,9 @@
 <template>
   <div>
-    <a target="_blank" @click="openInTab()">
-      <v-card class="doc-card mb-2" elevation="2" outlined max-width="350px">
+      <v-card class="doc-card mb-2 stephan-test" elevation="2" outlined max-width="350px" @click="openInTab()">
         <v-card-title class="text-subtitle-1 title">
           <div class="d-flex align-start flex-row">
-            <v-icon left size="30" class="mr-2">
+            <v-icon left size="30" class="mr-2" :aria-label="document.name">
               {{ icon }}
             </v-icon>
             {{ document.name }}
@@ -17,14 +16,16 @@
                 class="preview-component"
                 :src="document.data"
                 max-width="200px"
-                alt="Image preview..."
+                :alt="'Bildvorschau von ' + document.name"
             >
             </v-img>
 
             <vue2-pdf-embed
-                v-else-if="document.type === 'application/pdf'"
+                v-else-if="isPdf"
                 :source="document.data"
                 class="preview-component"
+                :aria-label="'PDF Vorschau von ' + document.name"
+
             />
 
             <div v-else class="preview-text">Keine Vorschau verfügbar</div>
@@ -36,6 +37,7 @@
                     elevation="1"
                     icon
                     @click.stop="removeDocument"
+                    :aria-label="document.name + ' entfernen'"
                 >
                   <v-icon> mdi-delete</v-icon>
                 </v-btn>
@@ -44,7 +46,6 @@
           </div>
         </v-card-text>
       </v-card>
-    </a>
   </div>
 </template>
 
@@ -52,6 +53,8 @@
 
 import {computed, defineComponent} from "vue";
 import {fileIcons} from "../util";
+import {formatBytes} from "@/middleware/fileSize";
+import {createBlobUrl} from "@/middleware/url";
 
 export default defineComponent({
   props: ['document', 'readonly'],
@@ -63,47 +66,11 @@ export default defineComponent({
     const icon = computed(() => fileIcons[props.document.type] ?? "mdi-file");
 
     const isImage = computed(() => props.document.type.toLowerCase() === "image/jpeg" || props.document.type.toLowerCase() === "image/png");
-
-    const blobUrl = (): string => {
-      const byteCharacters = calcByteCharacters.value;
-
-      const byteArrays: Uint8Array[] = [];
-
-      for (let offset = 0; offset < byteCharacters.length; offset += 1024) {
-        const slice = byteCharacters.slice(offset, offset + 1024);
-
-        const byteNumbers = new Array(slice.length);
-        for (let i = 0; i < slice.length; i++) {
-          byteNumbers[i] = slice.charCodeAt(i);
-        }
-        const byteArray = new Uint8Array(byteNumbers);
-        byteArrays.push(byteArray);
-      }
-      const blob = new Blob(byteArrays, {type: props.document.type});
-      return URL.createObjectURL(blob);
-    }
+    const isPdf = computed(() => props.document.type === 'application/pdf')
 
     const openInTab = () => {
-      const calcVlobUrl = blobUrl();
-      const link = document.createElement("a");
-      link.href = calcVlobUrl;
-      link.setAttribute("download", props.document.name!);
-      document.body.appendChild(link);
-      link.click();
-    }
-
-    const formatBytes = (decimals = 2) => {
-      const k = 1024;
-      const dm = decimals < 0 ? 0 : decimals;
-      const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
-
-      const i = Math.floor(Math.log(props.document.size) / Math.log(k));
-
-      return (
-          parseFloat((props.document.size / Math.pow(k, i)).toFixed(dm)) +
-          " " +
-          sizes[i]
-      );
+      const url = createBlobUrl(calcByteCharacters.value, props.document.type);
+      window.open(url, "_blank" )
     }
 
     const removeDocument = () => {
@@ -114,6 +81,7 @@ export default defineComponent({
       calcByteCharacters,
       icon,
       isImage,
+      isPdf,
       openInTab,
       formatBytes,
       removeDocument
