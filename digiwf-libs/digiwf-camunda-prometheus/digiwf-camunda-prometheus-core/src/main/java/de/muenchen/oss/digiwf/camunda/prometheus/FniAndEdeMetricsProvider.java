04ea7e66b6be26f9fa46fd6b4bf178d258033f91
Simon Hirtreiter <simon.hirtreiter@muenchen.de>
2024-04-10 15:51:27 +0200

    camunda metrics refactor fni and ede metric

--- digiwf-libs/digiwf-camunda-prometheus/digiwf-camunda-prometheus-core/src/main/java/de/muenchen/oss/digiwf/camunda/prometheus/FniAndEdeMetricsProvider.java
+++ digiwf-libs/digiwf-camunda-prometheus/digiwf-camunda-prometheus-core/src/main/java/de/muenchen/oss/digiwf/camunda/prometheus/FniAndEdeMetricsProvider.java
@@ -6,9 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.camunda.bpm.engine.HistoryService;
 import org.camunda.bpm.engine.ManagementService;
 import org.camunda.bpm.engine.RepositoryService;
-import org.camunda.bpm.engine.repository.ProcessDefinition;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 public class FniAndEdeMetricsProvider implements MetricsProvider {
@@ -23,16 +20,8 @@ public class FniAndEdeMetricsProvider implements MetricsProvider {
     @Override
     public void updateMetrics() {
         // Get total number of FNIs
-        this.fniCount
-            .labels("total", "total")
-            .set(this.managementService.createMetricsQuery().name("activity-instance-start").sum());
+        this.fniCount.set(this.managementService.createMetricsQuery().name("activity-instance-start").sum());
 
-        // Get FNIs by process definition (this only includes the currently deployed definitions and may be lower than the total count)
-        repositoryService.createProcessDefinitionQuery().list().forEach(processDefinition ->
-            this.fniCount
-                .labels(processDefinition.getKey(), processDefinition.getId())
-                .set(historyService.createHistoricActivityInstanceQuery().processDefinitionId(processDefinition.getId()).count())
-        );
         // Get total number of EDEs
         this.edeCount.set(this.managementService.createMetricsQuery().name("executed-decision-elements").sum());
     }
@@ -41,8 +30,7 @@ public class FniAndEdeMetricsProvider implements MetricsProvider {
     public void registerMetrics(final CollectorRegistry collectorRegistry) {
         this.fniCount = Gauge.build()
                 .name("camunda_activity_instances")
-                .help("Number of activity instances (BPMN FNI) in total and by deployed process definition.")
-                .labelNames("processDefinitionKey", "processDefinitionId")
+                .help("Number of activity instances (BPMN FNI).")
                 .register(collectorRegistry);
 
         this.edeCount = Gauge.build()
