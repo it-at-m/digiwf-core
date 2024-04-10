33c09366fad953a6dcbe938be7ad1279df180b4c
Simon Hirtreiter <simon.hirtreiter@muenchen.de>
2024-04-10 15:37:37 +0200

    camunda metrics refactor incident metric

--- digiwf-libs/digiwf-camunda-prometheus/digiwf-camunda-prometheus-core/src/main/java/de/muenchen/oss/digiwf/camunda/prometheus/IncidentMetricsProvider.java
+++ digiwf-libs/digiwf-camunda-prometheus/digiwf-camunda-prometheus-core/src/main/java/de/muenchen/oss/digiwf/camunda/prometheus/IncidentMetricsProvider.java
@@ -5,41 +5,32 @@ import io.prometheus.client.Gauge;
 import lombok.RequiredArgsConstructor;
 import org.camunda.bpm.engine.RepositoryService;
 import org.camunda.bpm.engine.RuntimeService;
-import org.camunda.bpm.engine.repository.ProcessDefinition;
-
-import static java.util.stream.Collectors.toMap;
+import org.camunda.bpm.engine.repository.ResourceDefinition;
 
 @RequiredArgsConstructor
 public class IncidentMetricsProvider implements MetricsProvider {
 
+    private static final String NOT_PROCESS_INCIDENT = "__no_process_definition_key";
     private final RuntimeService runtimeService;
     private final RepositoryService repositoryService;
-    private static final String NOT_PROCESS_INCIDENT = "__no_process_definition_key";
-
     private Gauge openIncidents;
 
     @Override
     public void updateMetrics() {
-        var processDefinitions = repositoryService.createProcessDefinitionQuery().list();
-        var incidentCounts = processDefinitions.stream().collect(toMap(ProcessDefinition::getKey, ignored -> 0L, Long::sum));
-        incidentCounts.put(NOT_PROCESS_INCIDENT, 0L);
-        var processDefinitionKeys = processDefinitions.stream().collect(toMap(ProcessDefinition::getId, ProcessDefinition::getKey));
-
-        runtimeService.createIncidentQuery().list().forEach(incident -> {
-            var processDefinitionKey = incident.getProcessDefinitionId() == null ? NOT_PROCESS_INCIDENT : processDefinitionKeys.get(incident.getProcessDefinitionId());
-            incidentCounts.merge(processDefinitionKey, 1L, Long::sum);
-        });
-        incidentCounts.forEach(
-            (processDefinitionKey, incidents) -> this.openIncidents.labels(processDefinitionKey).set(incidents));
+        repositoryService.createProcessDefinitionQuery().list().stream()
+                .map(ResourceDefinition::getKey).forEach((key) -> {
+                    openIncidents.labels(key).set(runtimeService.createIncidentQuery().processDefinitionKeyIn(key).count());
+                });
+        openIncidents.labels(NOT_PROCESS_INCIDENT).set(runtimeService.createIncidentQuery().processDefinitionId(null).count());
     }
 
     @Override
     public void registerMetrics(final CollectorRegistry collectorRegistry) {
         this.openIncidents = Gauge.build()
-            .name("camunda_incidents_open")
-            .labelNames("processDefinitionKey")
-            .help("Number of open incidents by process definition key.")
-            .register(collectorRegistry);
+                .name("camunda_incidents_open")
+                .labelNames("processDefinitionKey")
+                .help("Number of open incidents by process definition key.")
+                .register(collectorRegistry);
     }
 
 }
