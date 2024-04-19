c368ec0dc0eaea9bcdd91e6b07d4350371592d69
Simon Hirtreiter <simon.hirtreiter@muenchen.de>
2024-04-19 14:41:08 +0200

    engine-rest update mock adapter

--- digiwf-engine/digiwf-engine-rest-service/src/main/java/de/muenchen/oss/digiwf/adapter/out/ldap/LdapMockOutPort.java
+++ digiwf-engine/digiwf-engine-rest-service/src/main/java/de/muenchen/oss/digiwf/adapter/out/ldap/LdapMockOutPort.java
@@ -2,16 +2,19 @@ package de.muenchen.oss.digiwf.adapter.out.ldap;
 
 import de.muenchen.oss.digiwf.application.port.out.ResolveUserGroupsOutPort;
 import de.muenchen.oss.digiwf.domain.Group;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.lang.NonNull;
 
 import java.util.List;
 
+@Slf4j
 public class LdapMockOutPort implements ResolveUserGroupsOutPort {
     @Override
     @NonNull
     public List<Group> resolveGroups(@NonNull final String username) {
+        log.debug("Resolving groups for user via mock: {}", username);
         return List.of(
-                new Group("webapp-user")
+                new Group("digiwf-webapp-user")
         );
     }
 }
