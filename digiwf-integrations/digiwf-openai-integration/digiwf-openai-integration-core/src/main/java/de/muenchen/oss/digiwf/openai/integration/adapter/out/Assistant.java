275ea1c9cab01b6d48d73d3acd8084523a966a54
darenegade <darenegade@gmx.de>
2024-04-19 14:04:31 +0200

    Add Banner

--- digiwf-integrations/digiwf-openai-integration/digiwf-openai-integration-core/src/main/java/de/muenchen/oss/digiwf/openai/integration/adapter/out/Assistant.java
+++ digiwf-integrations/digiwf-openai-integration/digiwf-openai-integration-core/src/main/java/de/muenchen/oss/digiwf/openai/integration/adapter/out/Assistant.java
@@ -3,8 +3,7 @@ package de.muenchen.oss.digiwf.openai.integration.adapter.out;
 import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.spring.AiService;
 
-//https://learn.microsoft.com/de-de/azure/ai-services/openai/reference#chat-completions
-@AiService(chatModel = "gpt-3.5-turbo")
+@AiService
 public interface Assistant {
 
     @SystemMessage("You are a polite assistant")
