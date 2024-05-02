046f9c0775e9d0c23b971f2f8cc511729afe0de4
Julia Lehmann <julia.lehmann@muenchen.de>
2024-05-02 10:54:52 +0200

    #921: fix readonly errorMessage in GroupTaskDetail.vue

--- digiwf-apps/packages/apps/digiwf-tasklist/src/views/GroupTaskDetail.vue
+++ digiwf-apps/packages/apps/digiwf-tasklist/src/views/GroupTaskDetail.vue
@@ -151,8 +151,10 @@ const store = useStore();
 
 const isLoading = ref(false);
 
+const errorMessage = ref<string | null>();
+
 const {data: currentUser} = useCurrentUserInfo();
-const {data: taskLoadingResult, error: errorMessage, refetch: reload} = useTaskQuery(taskId);
+const {data: taskLoadingResult, error, refetch: reload} = useTaskQuery(taskId);
 
 const {
   mutateAsync: assignTask
@@ -172,6 +174,7 @@ onMounted(() => {
     }
     isLoading.value = false;
   });
+  errorMessage.value = error.value;
 });
 
 const checkTaskAssignment = () => {
@@ -185,7 +188,6 @@ const checkTaskAssignment = () => {
     }
   } else {
     triggerAssignTask();
-    router.push({path: "/task/" + taskId});
   }
 };
 
@@ -212,8 +214,8 @@ const triggerAssignTask = () => {
   }
   assignTask(lhmObjectId)
     .then(() => errorMessage.value)
-    .catch((error) => {
-      errorMessage.value = error;
+    .catch(() => {
+      errorMessage.value = "Aufgabe konnte nicht zugewiesen werden";
     });
 };
 
