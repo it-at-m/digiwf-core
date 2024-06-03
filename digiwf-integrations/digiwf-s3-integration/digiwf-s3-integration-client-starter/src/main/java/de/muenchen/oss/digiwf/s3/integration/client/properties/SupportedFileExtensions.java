5ae1bad2fc40bb3dabe6cb1d2ac204f51ab7f40e
Marko Streich <marko.streich@muenchen.de>
2024-06-03 08:37:16 +0200

    1442 remove redundant import

--- digiwf-integrations/digiwf-s3-integration/digiwf-s3-integration-client-starter/src/main/java/de/muenchen/oss/digiwf/s3/integration/client/properties/SupportedFileExtensions.java
+++ digiwf-integrations/digiwf-s3-integration/digiwf-s3-integration-client-starter/src/main/java/de/muenchen/oss/digiwf/s3/integration/client/properties/SupportedFileExtensions.java
@@ -1,7 +1,6 @@
 package de.muenchen.oss.digiwf.s3.integration.client.properties;
 
 import java.util.HashMap;
-import java.util.Map;
 
 public class SupportedFileExtensions extends HashMap<String, String> {
 }
