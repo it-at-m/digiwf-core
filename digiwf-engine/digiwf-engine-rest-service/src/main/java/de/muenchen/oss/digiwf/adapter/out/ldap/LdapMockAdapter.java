4ce95e84263b00532fb80c1dc8b3d45c8aab8626
Simon Hirtreiter <simon.hirtreiter@muenchen.de>
2024-06-10 11:08:37 +0200

    engine-rest init ldap resolve groups and users

--- digiwf-engine/digiwf-engine-rest-service/src/main/java/de/muenchen/oss/digiwf/adapter/out/ldap/LdapMockAdapter.java
+++ digiwf-engine/digiwf-engine-rest-service/src/main/java/de/muenchen/oss/digiwf/adapter/out/ldap/LdapMockAdapter.java
@@ -1,7 +1,11 @@
 package de.muenchen.oss.digiwf.adapter.out.ldap;
 
+import de.muenchen.oss.digiwf.application.port.out.GroupsOutPort;
 import de.muenchen.oss.digiwf.application.port.out.ResolveUserGroupsOutPort;
+import de.muenchen.oss.digiwf.application.port.out.ResolveUserOutPort;
 import de.muenchen.oss.digiwf.domain.Group;
+import de.muenchen.oss.digiwf.domain.User;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
 import org.springframework.lang.NonNull;
@@ -12,7 +16,7 @@ import java.util.List;
 @Component
 @Profile("groups-mock")
 @Slf4j
-public class LdapMockAdapter implements ResolveUserGroupsOutPort {
+public class LdapMockAdapter implements ResolveUserGroupsOutPort, ResolveUserOutPort, GroupsOutPort {
     @Override
     @NonNull
     public List<Group> resolveUserGroups(@NonNull final String username) {
@@ -20,5 +24,16 @@ public class LdapMockAdapter implements ResolveUserGroupsOutPort {
         return List.of(
                 new Group("digiwf-webapp-user")
         );
+    }
+
+    @Override
+    public User resolveUser(@NonNull final String username) {
+        return User.builder().id(username).build();
+    }
+
+    @NonNull
+    @Override
+    public List<String> getGroupsMembers(@NonNull @NotEmpty final List<Group> groups) {
+        return List.of("johndoe", "jandoe");
     }
 }
