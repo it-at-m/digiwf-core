97cbf7d869598e1ef651a5304c4f2b5fea9373b5
Marko Streich <marko.streich@muenchen.de>
2024-06-17 16:28:59 +0200

    higher cypress timeout

--- digiwf-cypress/cypress.config.js
+++ digiwf-cypress/cypress.config.js
@@ -7,7 +7,7 @@ require('dotenv').config({path: '.env.local'})
 module.exports = defineConfig({
     viewportHeight: 1200,
     viewportWidth: 2000,
-    defaultCommandTimeout: 10000,
+    defaultCommandTimeout: 20000,
     videosFolder: "output/videos",
     reporter: "cypress-multi-reporters",
     reporterOptions: {
