b630b2f2997ad8fb4d12cb1ec2b54cbf8004b2cc
darenegade <darenegade@users.noreply.github.com>
2024-07-01 10:01:49 +0200

    OpenAI integration (PoC) (#1741)
    * untested base implementation
    
    * First working Draft
    
    * Add Banner
    
    * Add RunConfig
    
    * Fix test log
    
    * More Functions
    
    * fix keycloak version
    
    * Add OpenAI Configs
    
    * Fix DTOs & Example Chat+Translate
    
    * Add Examples 4 summarize, extract, classify, generate mail
    
    * update digiwf version
    
    * Update Prompts & fix type names
    
    * Update Prompts & fix form
    
    * Basic messageprocessor tests
    
    * Basic ai outport tests
    
    * doku
    
    * readme
    
    * update version
    
    * add to workflow
    
    * Default Configs
    
    * Update docs/src/integrations/digiwf-openai-integration.md
    
    Co-authored-by: Simon Hirtreiter <simon.hirtreiter@muenchen.de>
    
    * Update docs/src/integrations/digiwf-openai-integration.md
    
    Co-authored-by: Simon Hirtreiter <simon.hirtreiter@muenchen.de>
    
    * Update docs/src/integrations/digiwf-openai-integration.md
    
    Co-authored-by: Simon Hirtreiter <simon.hirtreiter@muenchen.de>
    
    * Update docs/src/integrations/digiwf-openai-integration.md
    
    Co-authored-by: Simon Hirtreiter <simon.hirtreiter@muenchen.de>
    
    * Property Paths
    
    * add dumb whitespace 4 hash
    
    * move docs to modelling
    
    * fix docs path
    
    * remove mapJson
    
    * Add versions
    
    * merge dev
    
    ---------
    
    Co-authored-by: Simon Hirtreiter <simon.hirtreiter@muenchen.de>

---
+++ digiwf-integrations/digiwf-openai-integration/digiwf-openai-integration-core/src/main/java/de/muenchen/oss/digiwf/openai/integration/adapter/in/streaming/OpenAiMapper.java
@@ -0,0 +1,33 @@
+package de.muenchen.oss.digiwf.openai.integration.adapter.in.streaming;
+
+import de.muenchen.oss.digiwf.openai.integration.adapter.in.streaming.dto.ClassifyDto;
+import de.muenchen.oss.digiwf.openai.integration.adapter.in.streaming.dto.ExtractDataDto;
+import de.muenchen.oss.digiwf.openai.integration.adapter.in.streaming.dto.GenerateMailDto;
+import de.muenchen.oss.digiwf.openai.integration.adapter.in.streaming.dto.PromptDto;
+import de.muenchen.oss.digiwf.openai.integration.adapter.in.streaming.dto.SummarizeDto;
+import de.muenchen.oss.digiwf.openai.integration.adapter.in.streaming.dto.TranslateDto;
+import de.muenchen.oss.digiwf.openai.integration.adapter.out.dto.ChatRequest;
+import de.muenchen.oss.digiwf.openai.integration.adapter.out.dto.ClassifyRequest;
+import de.muenchen.oss.digiwf.openai.integration.adapter.out.dto.ExtractDataRequest;
+import de.muenchen.oss.digiwf.openai.integration.adapter.out.dto.GenerateMailRequest;
+import de.muenchen.oss.digiwf.openai.integration.adapter.out.dto.SummarizeRequest;
+import de.muenchen.oss.digiwf.openai.integration.adapter.out.dto.TranslateRequest;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface OpenAiMapper {
+
+
+    ChatRequest dto2Model(PromptDto promptDto);
+
+    TranslateRequest dto2Model(TranslateDto translateDto);
+
+    SummarizeRequest dto2Model(SummarizeDto summarizeDto);
+
+    GenerateMailRequest dto2Model(GenerateMailDto generateMailDto);
+
+    ExtractDataRequest dto2Model(ExtractDataDto extractDataDto);
+
+    ClassifyRequest dto2Model(ClassifyDto classifyDto);
+
+}
