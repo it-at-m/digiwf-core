973d23b8465dae52646fa218c2dc53d84f01dbe2
Simon Hirtreiter <simon.hirtreiter@muenchen.de>
2024-07-01 11:05:44 +0200

    unit-test lib init hex ArchUnit adapters not depending on each other

--- digiwf-libs/digiwf-testing/digiwf-unit-test/src/main/java/de/muenchen/oss/digiwf/archunit/HexagonalArchitectureTest.java
+++ digiwf-libs/digiwf-testing/digiwf-unit-test/src/main/java/de/muenchen/oss/digiwf/archunit/HexagonalArchitectureTest.java
@@ -4,11 +4,10 @@ import com.tngtech.archunit.base.DescribedPredicate;
 import com.tngtech.archunit.core.domain.JavaClass;
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.junit.ArchTest;
-import lombok.val;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
-import static java.util.stream.Collectors.groupingBy;
+import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices;
 
 public class HexagonalArchitectureTest {
     private static final String ADAPTER = "..adapter..";
@@ -77,36 +76,19 @@ public class HexagonalArchitectureTest {
         noClasses().that().resideInAPackage(ADAPTER_IN)
                 .should().dependOnClassesThat()
                 .resideInAnyPackage(APPLICATION_PORT_OUT, APPLICATION_USE_CASE, ADAPTER_OUT).check(classes);
-        testSubpackagesNotDependOnEachOther(classes.that(JavaClass.Predicates.resideInAPackage(ADAPTER_IN)));
+        slices().matching(ADAPTER_IN).should().notDependOnEachOther();
         // adapter out
         classes().that().resideInAPackage(ADAPTER_OUT).and().haveSimpleNameEndingWith("Adapter")
                 .should().implement(JavaClass.Predicates.resideInAPackage(APPLICATION_PORT_OUT)).check(classes);
         noClasses().that().resideInAPackage(ADAPTER_OUT)
                 .should().dependOnClassesThat()
                 .resideInAnyPackage(APPLICATION_PORT_IN, APPLICATION_USE_CASE, ADAPTER_IN).check(classes);
-        testSubpackagesNotDependOnEachOther(classes.that(JavaClass.Predicates.resideInAPackage(ADAPTER_OUT)));
+        slices().matching(ADAPTER_IN).should().notDependOnEachOther();
         // use case
         classes().that().resideInAPackage(APPLICATION_USE_CASE)
                 .should().implement(JavaClass.Predicates.resideInAPackage(APPLICATION_PORT_IN)).check(classes);
         noClasses().that().resideInAPackage(APPLICATION_USE_CASE)
                 .should().dependOnClassesThat()
                 .resideInAnyPackage(ADAPTER_OUT, ADAPTER_IN).check(classes);
-    }
-
-    void testSubpackagesNotDependOnEachOther(final JavaClasses classes) {
-        val classesByPackage = classes.stream().collect(groupingBy(JavaClass::getPackageName));
-        for (val pkg : classesByPackage.entrySet()) {
-            for (val pkg2 : classesByPackage.entrySet()) {
-                if (!pkg.getKey().equals(pkg2.getKey())) {
-                    noClasses()
-                            .that()
-                            .resideInAPackage(pkg.getKey())
-                            .should()
-                            .dependOnClassesThat()
-                            .resideInAnyPackage(pkg2.getKey())
-                            .check(classes);
-                }
-            }
-        }
     }
 }
