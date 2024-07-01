be480d74ef6a909c91b5416d143f9bea13869f3d
Simon Hirtreiter <simon.hirtreiter@muenchen.de>
2024-07-01 10:42:08 +0200

    example-integration refactor streaming response

--- digiwf-integrations/digiwf-example-integration/digiwf-example-integration-core/src/main/java/de/muenchen/oss/digiwf/example/integration/core/adapter/in/streaming/StreamingAdapter.java
+++ digiwf-integrations/digiwf-example-integration/digiwf-example-integration-core/src/main/java/de/muenchen/oss/digiwf/example/integration/core/adapter/in/streaming/StreamingAdapter.java
@@ -1,20 +1,27 @@
 package de.muenchen.oss.digiwf.example.integration.core.adapter.in.streaming;
 
 import de.muenchen.oss.digiwf.example.integration.core.application.port.in.ExampleInPort;
-import de.muenchen.oss.digiwf.example.integration.core.application.port.in.ProcessResponseInPort;
+import de.muenchen.oss.digiwf.message.process.api.ErrorApi;
+import de.muenchen.oss.digiwf.message.process.api.ProcessApi;
 import de.muenchen.oss.digiwf.message.process.api.error.BpmnError;
 import de.muenchen.oss.digiwf.message.process.api.error.IncidentError;
 import jakarta.validation.ValidationException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
 
 import java.util.Map;
+import java.util.Objects;
 import java.util.function.Consumer;
 
-@RequiredArgsConstructor
-public class MessageProcessor {
+import static de.muenchen.oss.digiwf.message.common.MessageConstants.*;
 
-    private final ProcessResponseInPort processResponseInPort;
+@RequiredArgsConstructor
+@Slf4j
+public class StreamingAdapter {
+
+    private final ErrorApi errorApi;
+    private final ProcessApi processApi;
     private final ExampleInPort exampleInPort;
     private final ExampleMapper exampleMapper;
 
@@ -23,7 +30,7 @@ public class MessageProcessor {
             final ExampleDto exampleDto = message.getPayload();
             this.exampleInPort.processExampleData(this.exampleMapper.toModel(exampleDto));
 
-            this.processResponseInPort.correlateMessage(
+            this.correlateMessage(
                     message.getHeaders(),
                     Map.of("someData", exampleDto.getSomeData())
             );
@@ -34,11 +41,19 @@ public class MessageProcessor {
         try {
             runnable.run();
         } catch (final BpmnError bpmnError) {
-            processResponseInPort.handleBpmnError(message.getHeaders(), bpmnError);
+            errorApi.handleBpmnError(message.getHeaders(), bpmnError);
         } catch (final IncidentError incidentError) {
-            processResponseInPort.handleIncident(message.getHeaders(), incidentError);
+            errorApi.handleIncident(message.getHeaders(), incidentError);
         } catch (final ValidationException validationException) {
-            processResponseInPort.handleIncident(message.getHeaders(), new IncidentError(validationException.getMessage()));
+            errorApi.handleIncident(message.getHeaders(), new IncidentError(validationException.getMessage()));
         }
+    }
+
+    private void correlateMessage(final Map<String, Object> originMessageHeaders, final Map<String, Object> message) {
+        String processInstanceId = Objects.requireNonNull(originMessageHeaders.get(DIGIWF_PROCESS_INSTANCE_ID).toString());
+        String type = Objects.requireNonNull(originMessageHeaders.get(TYPE).toString());
+        String integrationName = Objects.requireNonNull(originMessageHeaders.get(DIGIWF_INTEGRATION_NAME).toString());
+        log.info("sending response message for process {}: {}", processInstanceId, message);
+        this.processApi.correlateMessage(processInstanceId, type, integrationName, message);
     }
 }
--- digiwf-integrations/digiwf-example-integration/digiwf-example-integration-starter/src/main/java/de/muenchen/oss/digiwf/example/integration/ExampleIntegrationAutoconfiguration.java
+++ digiwf-integrations/digiwf-example-integration/digiwf-example-integration-starter/src/main/java/de/muenchen/oss/digiwf/example/integration/ExampleIntegrationAutoconfiguration.java
@@ -1,10 +1,8 @@
 package de.muenchen.oss.digiwf.example.integration;
 
 import de.muenchen.oss.digiwf.example.integration.core.application.port.in.ExampleInPort;
-import de.muenchen.oss.digiwf.example.integration.core.application.port.in.ProcessResponseInPort;
-import de.muenchen.oss.digiwf.example.integration.core.application.port.out.ProcessResponseOutPort;
+import de.muenchen.oss.digiwf.example.integration.core.application.port.out.ExampleOutPort;
 import de.muenchen.oss.digiwf.example.integration.core.application.usecase.ExampleUseCase;
-import de.muenchen.oss.digiwf.example.integration.core.application.usecase.ProcessResponseUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
@@ -15,15 +13,10 @@ import org.springframework.context.annotation.Configuration;
 @RequiredArgsConstructor
 @ComponentScan(basePackages = {"de.muenchen.oss.digiwf.example.integration.core"})
 public class ExampleIntegrationAutoconfiguration {
-    @ConditionalOnMissingBean
-    @Bean
-    public ProcessResponseInPort processResponseInPort(final ProcessResponseOutPort processResponseOutPort) {
-        return new ProcessResponseUseCase(processResponseOutPort);
-    }
 
     @ConditionalOnMissingBean
     @Bean
-    public ExampleInPort exampleInPort() {
-        return new ExampleUseCase();
+    public ExampleInPort exampleInPort(final ExampleOutPort exampleOutPort) {
+        return new ExampleUseCase(exampleOutPort);
     }
 }
